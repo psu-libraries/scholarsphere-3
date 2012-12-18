7ed46cece6525f227e890bd5e04c90a30f924c5a
Joni Barnoff <jxb13@psu.edu>
2012-12-18 12:55:00 -0500

    Added javascript file sufia-psu-changes which display PSU shield and scholarsphere brand in header.  Added css file sufia-psu-changes to eliminate padding in body, top.  Keeping separate files until changes are complete

---
+++ app/assets/javascripts/sufia-psu-changes.js
@@ -0,0 +1,9 @@
+//Changes for sufia
+
+$(function(){
+$('a.brand.shield').replaceWith('<a id="shield" href="http://psu.edu/"> <img width="219" height="60" alt="Penn State University" src="/assets/site_images/logo_psuss_shield.png"></a>');
+});
+
+$(function(){
+$('a#logo').replaceWith('<a id="logo" href="/"> <img width="132" height="37" alt="Penn State ScholarSphere" src="/assets/site_images/logo_psuss_logotype.png"> </a>');
+});
