c1afb4be062accb900549d4fa5f1b1502b1ed36a
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2013-04-18 15:13:59 -0400

    Disable FITS metadata mapping: fixes #3738, refs #3733

--- config/application.rb
+++ config/application.rb
@@ -27,15 +27,12 @@ Bundler.require *Rails.groups(:assets => %w(development, test))
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-Sufia::Engine.configure do
-    # email to send on contact form - probably need one for the production
-    # environment
-    config.contact_email = 'DLT-GAMMA-PROJECT@lists.psu.edu'
-    config.from_email = "ScholarSphere Form <scholarsphere-service-support@dlt.psu.edu>"
-end
-
 module ScholarSphere
   class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
     # Returns an array containing the vhost 'CoSign service' value and URL
     def get_vhost_by_host
       hostname = Socket.gethostname
@@ -45,36 +42,13 @@ module ScholarSphere
       service << "-#{port}" unless port == 443
       return [service, vhost]
     end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.fits_path = 'fits.sh'
-    config.max_days_between_audits = 7
-    config.id_namespace = "scholarsphere"
-    config.application_name = "ScholarSphere"
-    config.persistent_hostpath = "http://scholarsphere.psu.edu/files/"
-    # turning on the new asset pipeline for handling javascript, css, and image files
-    config.assets.enabled = true
-    # Default SASS Configuration, check out https://github.com/rails/sass-rails for details
-    config.assets.compress = !Rails.env.development?
-
-    config.assets.paths << '#{Rails.root}/app/assets/javascripts'
-    config.assets.paths << '#{Rails.root}/app/assets/stylesheets'
-    config.assets.paths << '#{Rails.root}/app/assets/images'
-    config.assets.paths << '#{Rails.root}/lib/assets/javascripts'
-    config.assets.paths << '#{Rails.root}/lib/assets/stylesheets'
-    config.assets.paths << '#{Rails.root}/lib/assets/images'
-    config.assets.paths << '#{Rails.root}/vendor/assets/javascripts'
-    config.assets.paths << '#{Rails.root}/vendor/assets/images'
-    config.assets.paths << '#{Rails.root}/vendor/assets/fonts'
 
     config.scholarsphere_version = "v1.5.0"
     config.scholarsphere_release_date = "April 30, 2013"
-
-    config.fits_to_desc_mapping = {
-      :file_title => :title,
-      :file_author => :creator
-    }
+    config.id_namespace = "scholarsphere"
+    config.persistent_hostpath = "http://scholarsphere.psu.edu/files/"
+    # # of fits array items shown on the Generic File show page
+    config.fits_message_length = 5
 
     # Map hostnames onto Google Analytics tracking IDs
     config.ga_host_map = {
@@ -82,7 +56,6 @@ module ScholarSphere
       'scholarsphere.psu.edu' => 'UA-33252017-2',
     }
 
-    # Map hostnames onto vhosts
     config.hosts_vhosts_map = {
       'ss1test' => 'https://scholarsphere-integration.dlt.psu.edu:8443/',
       'ss2test' => 'https://scholarsphere-test.dlt.psu.edu/',
@@ -92,76 +65,17 @@ module ScholarSphere
       'ss2prod' => 'https://scholarsphere.psu.edu/'
     }
 
-    config.resource_types = {
-      "Article" => "Article",
-      "Audio" => "Audio",
-      "Book" => "Book",
-      "Capstone Project" => "Capstone Project",
-      "Conference Proceeding" => "Conference Proceeding",
-      "Dataset" => "Dataset",
-      "Dissertation" => "Dissertation",
-      "Image" => "Image",
-      "Journal" => "Journal",
-      "Map or Cartographic Material" => "Map or Cartographic Material",
-      "Masters Thesis" => "Masters Thesis",
-      "Part of Book" => "Part of Book",
-      "Poster" => "Poster",
-      "Presentation" => "Presentation",
-      "Project" => "Project",
-      "Report" => "Report",
-      "Research Paper" => "Research Paper",
-      "Software or Program Code" => "Software or Program Code",
-      "Video" => "Video",
-      "Other" => "Other",
-    }
-
-    config.public_permission_levels = {
-      "Choose Access"=>"none",
-      "View/Download" => "read"
-    }
-
-    config.permission_levels = {
-      "Choose Access"=>"none",
-      "View/Download" => "read",
-      "Edit" => "edit"
-    }
-
-    config.owner_permission_levels = {
-      "Edit" => "edit"
-    }
-
-    config.cc_licenses = {
-      'Attribution 3.0 United States' => 'http://creativecommons.org/licenses/by/3.0/us/',
-      'Attribution-ShareAlike 3.0 United States' => 'http://creativecommons.org/licenses/by-sa/3.0/us/',
-      'Attribution-NonCommercial 3.0 United States' => 'http://creativecommons.org/licenses/by-nc/3.0/us/',
-      'Attribution-NoDerivs 3.0 United States' => 'http://creativecommons.org/licenses/by-nd/3.0/us/',
-      'Attribution-NonCommercial-NoDerivs 3.0 United States' => 'http://creativecommons.org/licenses/by-nc-nd/3.0/us/',
-      'Attribution-NonCommercial-ShareAlike 3.0 United States' => 'http://creativecommons.org/licenses/by-nc-sa/3.0/us/',
-      'Public Domain Mark 1.0' => 'http://creativecommons.org/publicdomain/mark/1.0/',
-      'CC0 1.0 Universal' => 'http://creativecommons.org/publicdomain/zero/1.0/',
-      'All rights reserved' => 'All rights reserved'
-    }
-
-    config.cc_licenses_reverse = Hash[*config.cc_licenses.to_a.flatten.reverse]
-
-    # help text to display for form metadata elements, these will need to be updated to
-    # reflect a field name change (should one happen) in the generic file datastream
-    config.metadata_help = {
-      "generic_file_resource_type" => "Pre-defined categories to describe the type of file content being uploaded, such as \"article\" or \"dataset.\"  More than one type may be selected.",
-      "generic_file_title" => "A name for the file to aid in identifying it. Defaults to the file name, though a more descriptive title is encouraged. <em>This is a required field</em>.",
-      "generic_file_tag" => "Words or phrases you select to describe what the file is about. These are used to search for content. <em>This is a required field</em>.",
-      "generic_file_subject" => "Headings or index terms describing what the file is about; these <em>do</em> need to conform to an existing vocabulary. Currently ScholarSphere supports Library of Congress Subject Headings.",
-      "generic_file_creator" => "The person or group responsible for the file being uploaded. Usually this is the author of the content. Personal names should be entered with the last name first, e.g. \"Smith, John.\" <em>This is a required field</em>.",
-      "generic_file_related_url" => "A link to a website or other specific content (audio, video, PDF document) related to the file. An example is the URL of a research project from which the file was derived.",
-      "generic_file_based_near" => "A place name related to the file, such as its site of publication, or the city, state, or country the file's contents are about. Calls upon the GeoNames web service (<a href=\"http://www.geonames.org\">http://www.geonames.org</a>).",
-      "generic_file_contributor" => "A person or group you want to recognize for playing a role in the creation of the file, but not the primary role. If there is a specific role you would like noted, include it in parentheses, e.g. \"Jones, Mary (advisor).\"",
-      "generic_file_date_created" => "The date on which the file was generated. Dates are accepted in the form YYYY-MM-DD, e.g. 1776-07-04.",
-      "generic_file_description" => "Free-text notes about the file itself. Examples include abstracts of a paper, citation information for a journal article, or a tag indicating a larger collection to which the file belongs.",
-      "generic_file_identifier" => "A unique handle describing the file. An example would be a DOI for a journal article, or an ISBN or OCLC number for a book.",
-      "generic_file_language" => " The language of the file content.",
-      "generic_file_publisher" => "The person or group making the file available. Generally this is Penn State or the Penn State University Libraries.",
-      "generic_file_rights" => "Licensing and distribution information governing access to the file. Select from the provided drop-down list. <em>This is a required field</em>."
-    }
+    config.assets.enabled = true
+    config.assets.compress = !Rails.env.development?
+    config.assets.paths << '#{Rails.root}/app/assets/javascripts'
+    config.assets.paths << '#{Rails.root}/app/assets/stylesheets'
+    config.assets.paths << '#{Rails.root}/app/assets/images'
+    config.assets.paths << '#{Rails.root}/lib/assets/javascripts'
+    config.assets.paths << '#{Rails.root}/lib/assets/stylesheets'
+    config.assets.paths << '#{Rails.root}/lib/assets/images'
+    config.assets.paths << '#{Rails.root}/vendor/assets/javascripts'
+    config.assets.paths << '#{Rails.root}/vendor/assets/images'
+    config.assets.paths << '#{Rails.root}/vendor/assets/fonts'
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += Dir["#{config.root}/app/models/**/"]
@@ -190,9 +104,6 @@ module ScholarSphere
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
-    #configure the number of fits array items shown on the Generic File show page
-    config.fits_message_length = 5
   end
 end
 
--- spec/models/generic_file_spec.rb
+++ spec/models/generic_file_spec.rb
@@ -448,17 +448,17 @@ describe GenericFile do
         @myfile.characterization_terms.keys.should include(:last_modified)
         @myfile.characterization_terms.keys.should include(:filename)
       end
-      it "should append metadata from the characterization" do
-        @myfile.title.should include("Microsoft Word - sample.pdf.docx")
+      it "should NOT append metadata from the characterization" do
+        @myfile.title.should_not include("Microsoft Word - sample.pdf.docx")
         @myfile.filename[0].should == @myfile.label
       end
       it "should include thumbnail generation in characterization job" do
         @myfile.thumbnail.size.nil?.should be_false
       end
-      it "should append each term only once" do
+      it "should NOT append each term only once" do
         @myfile.append_metadata
         @myfile.format_label.should == ["Portable Document Format"]
-        @myfile.title.should include("Microsoft Word - sample.pdf.docx")
+        @myfile.title.should_not include("Microsoft Word - sample.pdf.docx")
       end
       it "should include extracted text" do
         @myfile.full_text.content.should == "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nMicrosoft Word - sample.pdf.docx\n\n\n \n \n\n \n\n \n\n \n\nThis PDF file was created using CutePDF. \n\nwww.cutepdf.com \n\n\n\n"
