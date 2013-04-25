db73386b4d9565fe6beb2e6744529b8ef1f4ab13
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2013-04-25 16:16:21 -0400

    Sped up the test suite (e.g. by removing unnecessary or inefficient tests such as the nitty-gritty Sufia::IdService ones): fixes #3777

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -15,50 +15,43 @@
 require 'spec_helper'
 
 describe StatsController do
+  before do
+    User.any_instance.stubs(:groups).returns(["umg/dlt.scholarsphere-admin-viewers"])
+    User.any_instance.stubs(:name).returns("Jill Z. User")
 
+    # GenericFile.any_instance.stubs(:characterize_if_changed).yields
+    @user = FactoryGirl.find_or_create(:user)
+    @user2 = FactoryGirl.find_or_create(:archivist)
+    @user3 = FactoryGirl.find_or_create(:test_user_1)
+    @user4 = FactoryGirl.find_or_create(:test_user_2)
+    @user5 = FactoryGirl.find_or_create(:curator)
+    sign_in @user
+    sign_in @user2
+    sign_in @user3
+    sign_in @user4
+    sign_in @user5
 
-  before  do
-#     GenericFile.any_instance.stubs(:characterize_if_changed).yields
-     @user = FactoryGirl.find_or_create(:user)
-     @user2=FactoryGirl.find_or_create(:archivist)
-     @user3=FactoryGirl.find_or_create(:test_user_1)
-     @user4=FactoryGirl.find_or_create(:test_user_2)
-     @user5=FactoryGirl.find_or_create(:curator)
-     sign_in @user
-     sign_in @user2
-     sign_in @user3
-     sign_in @user4
-     sign_in @user5
-
-     User.any_instance.stubs(:groups).returns(["umg/dlt.scholarsphere-admin-viewers"])
-
-#    controller.stubs(:clear_session_user) ## Don't clear out the authenticated session
-     User.any_instance.stubs(:name).returns("Jill Z. User")
-     sleep 1
-     @gf1 =  GenericFile.new(title:'Test Document PDF', filename:'test4.pdf', read_groups:['public'], format_label:['Portable Document Format'], creator:['curator1'])
-     @gf1.apply_depositor_metadata(@user5.login)
-     @gf1.save
-     sleep 1
-     @gf2 =  GenericFile.new(title:'Test 2 Document', filename:'test2.docx', contributor:'Contrib2', read_groups:['public'], format_label:['Microsoft Word', 'FPX'], creator:['jilluser'] )
-     @gf2.apply_depositor_metadata(@user.login)
-     @gf2.save
-     sleep 1
-     @gf3 =  GenericFile.new(title:'Test Document image', filename:'test6.jp2', read_groups:['public'], format_label:['Exchangeable Image File Format'], creator:['archivist1'])
-     @gf3.apply_depositor_metadata(@user2.login)
-     @gf3.save
-     sleep 1
-     @gf4 =  GenericFile.new(title:'Test Text', filename:'test5.txt', contributor:'Contrib2', read_groups:['public'], format_label:['OpenDocument Text'], creator:['archivist1'])
-     @gf4.apply_depositor_metadata(@user2.login)
-     @gf4.save
-     @gf5 =  GenericFile.new(title:'Test Text', filename:'test3.xls', contributor:'Contrib2', read_groups:['public'], creator:['tstem31'])
-     @gf5.apply_depositor_metadata(@user3.login)
-     @gf5.save
-     @gf7 =  GenericFile.new(title:'Test Text', filename:'test1.txt', contributor:'Contrib2', read_groups:['public'], format_label:['OpenDocument Text'], creator:['testapp'])
-     @gf7.apply_depositor_metadata(@user4.login)
-     @gf7.save
-
-    end
-    after  do
+    # controller.stubs(:clear_session_user) ## Don't clear out the authenticated session
+    @gf1 =  GenericFile.new(title:'Test Document PDF', filename:'test4.pdf', read_groups:['public'], format_label:['Portable Document Format'], creator:['curator1'])
+    @gf1.apply_depositor_metadata(@user5.login)
+    @gf1.save
+    @gf2 =  GenericFile.new(title:'Test 2 Document', filename:'test2.docx', contributor:'Contrib2', read_groups:['public'], format_label:['Microsoft Word', 'FPX'], creator:['jilluser'] )
+    @gf2.apply_depositor_metadata(@user.login)
+    @gf2.save
+    @gf3 =  GenericFile.new(title:'Test Document image', filename:'test6.jp2', read_groups:['public'], format_label:['Exchangeable Image File Format'], creator:['archivist1'])
+    @gf3.apply_depositor_metadata(@user2.login)
+    @gf3.save
+    @gf4 =  GenericFile.new(title:'Test Text', filename:'test5.txt', contributor:'Contrib2', read_groups:['public'], format_label:['OpenDocument Text'], creator:['archivist1'])
+    @gf4.apply_depositor_metadata(@user2.login)
+    @gf4.save
+    @gf5 =  GenericFile.new(title:'Test Text', filename:'test3.xls', contributor:'Contrib2', read_groups:['public'], creator:['tstem31'])
+    @gf5.apply_depositor_metadata(@user3.login)
+    @gf5.save
+    @gf6 =  GenericFile.new(title:'Test Text', filename:'test1.txt', contributor:'Contrib2', read_groups:['public'], format_label:['OpenDocument Text'], creator:['testapp'])
+    @gf6.apply_depositor_metadata(@user4.login)
+    @gf6.save
+  end
+  after do
       @user.delete
       @user2.delete
       @user3.delete
@@ -69,19 +62,16 @@ describe StatsController do
       @gf3.delete
       @gf4.delete
       @gf5.delete
-      @gf7.delete
+      @gf6.delete
     end
 
-describe "statistics page" do
-
-      render_views
-
-      it "renders the statstics list view" do
-         get :list
-#         response.should be_success
-         response.should_not redirect_to(root_path)
-         response.body.should include('Statistics for ScholarSphere')
-         response.body.should include('Total Scholarsphere Members')
-      end
-   end
+  describe "statistics page" do
+    render_views
+    it "renders the statstics list view" do
+      get :list
+      response.should_not redirect_to(root_path)
+      response.body.should include('Statistics for ScholarSphere')
+      response.body.should include('Total Scholarsphere Members')
+    end
+  end
 end
--- spec/lib/sufia/id_service_spec.rb
+++ spec/lib/sufia/id_service_spec.rb
@@ -15,6 +15,9 @@
 require 'spec_helper'
 
 describe Sufia::IdService do
+  it "should respond to mint" do
+    Sufia::IdService.should respond_to(:mint)
+  end
   describe "mint" do
     before(:all) do
       @id = Sufia::IdService.mint
@@ -22,47 +25,12 @@ describe Sufia::IdService do
     it "should create a unique id" do
       @id.should_not be_empty
     end
+    it "should look like a ScholarSphere id" do
+      @id.should match(/scholarsphere\:.{9}/)
+    end
     it "should not mint the same id twice in a row" do
       other_id = Sufia::IdService.mint
       other_id.should_not == @id
-    end  
-    it "should create many unique ids" do
-      a = []
-      threads = (1..10).map do
-        Thread.new do
-          100.times do
-            a <<  Sufia::IdService.mint
-          end
-        end
-      end
-      threads.each(&:join)     
-      a.uniq.count.should == a.count
-    end
-    it "should create many unique ids when hit by multiple processes " do
-      rd, wr = IO.pipe
-      2.times do
-        pid = fork do 
-          rd.close
-          threads = (1..10).map do
-            Thread.new do
-              20.times do
-                wr.write Sufia::IdService.mint
-                wr.write " "
-              end
-            end
-          end
-          threads.each(&:join)        
-          wr.close
-        end
-      end
-      wr.close
-      2.times do
-        Process.wait
-      end
-      s = rd.read
-      rd.close
-      a = s.split(" ")
-      a.uniq.count.should == a.count
     end
   end
 end
