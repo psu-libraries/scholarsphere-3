ee9daafbea62229055fb1b7cd5d2b4396d2c3cd8
Carolyn Cole <cam156@psu.edu>
2013-04-30 10:50:30 -0400

    fixing solr_document to gte the hydra model correctly

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -7,6 +7,6 @@ class SolrDocument
 
   # Method to return the ActiveFedora model
   def hydra_model
-    Array(self[Solrizer.solr_name('has_model', :symbol)]).first
+    Array(self[Solrizer.solr_name('active_fedora_model', :symbol)]).first
   end
 end
