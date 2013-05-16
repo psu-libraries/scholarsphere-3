cde62025c8cc8b365ab2c5f03b63d338432c219b
Dan Coughlin <dan.coughlin@gmail.com>
2013-05-16 08:04:29 -0400

    uses psu stewardship sufia to implement after delivery hook for sending configured contact form emails fixes #3739

--- Gemfile
+++ Gemfile
@@ -11,7 +11,8 @@ gem 'rails', '3.2.13'
 gem 'mysql2', '0.3.11'
 
 # Hydra community components
-gem 'sufia', :github => 'projecthydra/sufia', :ref => '4568b1b6a6ed503c5082eab9ab6c750aeaaa8a46'
+#gem 'sufia', :github => 'projecthydra/sufia', :ref => '4568b1b6a6ed503c5082eab9ab6c750aeaaa8a46'
+gem 'sufia', :github => 'psu-stewardship/sufia', :ref => '69765f610ad717959ce2e3c43203d2d1942bca70'
 gem 'hydra-ldap', '0.1.0'
 gem 'jquery-rails', '2.1.4'
 gem 'resque-pool', '0.3.0'
--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -3,6 +3,20 @@ require 'sufia/http_header_auth'
 Sufia::Engine.configure do
   config.application_name = "ScholarSphere"
   config.google_analytics_id = Rails.application.google_analytics_id
+  config.enable_contact_form_delivery = true
+  config.contact_form_delivery_body = <<-EOM
+    Greetings,
+
+    Thank you for contacting us with your question or issue about ScholarSphere.
+    Our service team will review the submitted form. Within three business days after submission,
+    a member of the team will get back to you via email with a more specific response.
+    If this is an urgent concern, please consult this page, https://scholarsphere.psu.edu/help/.
+
+    Sincerely,
+
+    The ScholarSphere Service Team
+  EOM
+  config.contact_form_delivery_from = 'do-not-reply@scholarsphere.psu.edu'
   config.fits_path = "fits.sh"
   config.fits_to_desc_mapping= {}
   config.max_days_between_audits = 7
