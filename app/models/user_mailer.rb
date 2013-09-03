68f7ca5d6707a4735647d772a6284318bd1f84d7
Carolyn Cole <cam156@psu.edu>
2013-09-03 12:19:26 -0400

    Integrating email format from communivations

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,8 +2,12 @@ class UserMailer < ActionMailer::Base
 
   def response_for_information(email, name)
     @view_data = {email:email, name:name}
+
     attachments['scholarsphere-information.pdf'] = File.read(Rails.root.join('public/scholarsphere-landing-response.pdf'))
-    attachments.inline['logo.png'] = File.read(Rails.root.join('app/assets/images/site_images/logo_psuss_shield.png'))
+    attachments.inline['penn-state-mark.png'] = File.read(Rails.root.join('app/assets/images/penn-state-mark.png'))
+    attachments.inline['scholarshere-logo.png'] = File.read(Rails.root.join('app/assets/images/scholarshere-logo.png'))
+    attachments.inline['pennstate-it.png'] = File.read(Rails.root.join('app/assets/images/pennstate-it.png'))
+    attachments.inline['universitylibraries.png'] = File.read(Rails.root.join('app/assets/images/universitylibraries.png'))
     mail( to: email, cc:ScholarSphere::Application.config.landing_email, from: ScholarSphere::Application.config.landing_from_email, subject: "ScholarSphere Information Response")
   end
 end
