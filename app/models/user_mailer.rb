3f8841978bccedd328a48cabdbd5070f159405b5
Carolyn Cole <cam156@psu.edu>
2013-09-09 11:57:53 -0400

    Moving from inline logos to externally referenced logos

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -4,10 +4,6 @@ class UserMailer < ActionMailer::Base
     @view_data = {email:email, first_name:first_name}
 
     attachments['scholarsphere-information.pdf'] = File.read(Rails.root.join('public/scholarsphere-landing-response.pdf'))
-    attachments.inline['penn-state-mark.png'] = File.read(Rails.root.join('app/assets/images/penn-state-mark.png'))
-    attachments.inline['scholarshere-logo.png'] = File.read(Rails.root.join('app/assets/images/scholarshere-logo.png'))
-    attachments.inline['pennstate-it.png'] = File.read(Rails.root.join('app/assets/images/pennstate-it.png'))
-    attachments.inline['universitylibraries.png'] = File.read(Rails.root.join('app/assets/images/universitylibraries.png'))
     mail( to: email, bcc:ScholarSphere::Application.config.landing_email, from: ScholarSphere::Application.config.landing_from_email, subject: "ScholarSphere - We're here to help")
   end
 end
