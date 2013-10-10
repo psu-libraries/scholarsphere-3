392ba5546f76face09ba3dc768f253cefd668712
Jeff Minelli <jgm106@psu.edu>
2013-10-10 13:46:49 -0400

    wheneverifying cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,13 @@
 # Use this file to easily define all of your cron jobs.
- set :output, "#{path}/log/wheneveroutput.log"
+set :output, "#{path}/log/wheneveroutput.log"
 
-# every 1.minute do   
-#     rake "dbhosting:update_pg_hba_conf"
-# end
+every :day, :at => "12:00am" do   
+  command "/dlt/scholarsphere/bin/whenever_generate_sitemap.sh"
+end
+
+every :day, :at => "12:20am" do   
+  command "/dlt/scholarsphere/bin/whenever_audit_repository.sh"
+end
 
 
 # Learn more: http://github.com/javan/whenever
