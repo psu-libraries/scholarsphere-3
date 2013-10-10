5faa27bad58db9593569f078a447a346f10cf43a
Jeff Minelli <jgm106@psu.edu>
2013-10-10 12:19:13 -0400

    adding whenever config

---
+++ config/schedule.rb
@@ -0,0 +1,9 @@
+# Use this file to easily define all of your cron jobs.
+ set :output, "#{path}/log/wheneveroutput.log"
+
+# every 1.minute do   
+#     rake "dbhosting:update_pg_hba_conf"
+# end
+
+
+# Learn more: http://github.com/javan/whenever
