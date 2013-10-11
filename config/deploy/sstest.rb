80bc8da12c5e051c98b8ad88982c7eac0b96fa26
Jeff Minelli <jgm106@psu.edu>
2013-10-11 09:10:24 -0400

    added demo + production role files for capistrano

--- config/deploy/sstest.rb
+++
@@ -1,5 +0,0 @@
-role :web, "ss2test.dlt.psu.edu"
-role :app, "ss2test.dlt.psu.edu"
-role :solr, "ss2test.dlt.psu.edu" # This is where resolrize will run
-role :db,  "ss2test.dlt.psu.edu", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
