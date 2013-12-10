c14e8948a63c7aa58497e4ab092d40af6decef35
Justin Coyne <justin@curationexperts.com>
2013-12-10 16:26:47 -0600

    Ensure that render_facet_value is being called with the correct url helper. Fixes #8974

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,19 +1,7 @@
 require 'spec_helper'
 
 module Dashboard
-  
-  describe_options = {type: :feature}
-  
-  describe 'view_dashboard', describe_options do
-    before(:each) do
-      Warden.test_mode!
-      @old_resque_inline_value = Resque.inline
-      Resque.inline = true
-    end
-    after(:each) do
-     Warden.test_reset!
-     Resque.inline = @old_resque_inline_value
-    end
+  describe 'view_dashboard' do
     after(:all) do
       GenericFile.destroy_all
       Collection.destroy_all
@@ -21,15 +9,17 @@ module Dashboard
     let(:user) { FactoryGirl.find_or_create(:user) }
   
     describe 'visit dashboard' do
-      it "should visit dashboard" do
+      before do
         @collection = Collection.new title:'collection title'
         @collection.description = 'collection description'
         @collection.apply_depositor_metadata(user.user_key)
-        @collection.save
-        @gf1 =  GenericFile.new title: 'file title' 
+        @collection.save!
+        @gf1 =  GenericFile.new title: 'file title', resource_type: 'Video' 
         @gf1.apply_depositor_metadata(user.user_key)
-        @gf1.save
+        @gf1.save!
+      end
 
+      it "should visit dashboard" do
         login_as (user.user_key)
         visit ('/')
         click_link('my dashboard')
