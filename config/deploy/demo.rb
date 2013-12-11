c845332b4e311811c87c7da6de1455ca661d7be0
Jeff Minelli <jgm106@psu.edu>
2013-12-11 12:31:25 -0500

    cleaning up cap env.

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,5 +1,5 @@
-role :web, "ss3test.dlt.psu.edu"
-role :app, "ss3test.dlt.psu.edu"
-role :solr, "ss3test.dlt.psu.edu" # This is where resolrize will run
-role :db,  "ss3test.dlt.psu.edu", :primary => true # This is where Rails migrations will run
+role :web, "ss1demo.dlt.psu.edu"
+role :app, "ss1demo.dlt.psu.edu"
+role :solr, "ss1demo.dlt.psu.edu" # This is where resolrize will run
+role :db,  "ss1demo.dlt.psu.edu", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
---
+++ config/deploy/test.tb
@@ -0,0 +1,5 @@
+role :web, "ss2test.dlt.psu.edu"
+role :app, "ss2test.dlt.psu.edu"
+role :solr, "ss2test.dlt.psu.edu" # This is where resolrize will run
+role :db,  "ss2test.dlt.psu.edu", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
