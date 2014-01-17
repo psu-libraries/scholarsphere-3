7cf29437e232025f0b141c05fa83bb83b461d5a3
Carolyn Cole <cam156@psu.edu>
2014-01-17 11:30:18 -0500

    adding feature test to make sure the user's deposits are showing correctly.  refs #9004

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,17 +1,34 @@
 require 'spec_helper'
 
 describe "User Profile" do
+  let(:user_name) {"curator1"}
+  let(:conn) { ActiveFedora::SolrService.instance.conn }
+  before do
+    # More than 10 times, because the pagination threshold is 10
+    12.times do |t|
+      conn.add  :id => "199#{t}", Solrizer.solr_name('depositor', :stored_searchable) => user_name
+    end
+    conn.commit
+  end
+  after do
+    12.times do |t|
+      conn.delete_by_id "199#{t}"
+    end
+    conn.commit
+  end
 
   before do
     # TODO: This really shouldn't be necessary
     unspoof_http_auth
     sign_in :curator
+
     visit "/"
     click_link "curator1"
   end
 
   it "should be displayed" do
     page.should have_content "Edit Your Profile"
+    page.should have_content "Deposited Files 12"
   end
 
   it "should be editable" do
