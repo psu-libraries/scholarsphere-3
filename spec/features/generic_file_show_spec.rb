c54220334c57a5aafb9045df8b02f8e358d7582b
Carolyn Cole and Chet Swalina <developers+carolyn+chet@westarete.com>
2014-01-30 12:12:55 -0500

    Begin displaying all generic file show links refs #9073

--- spec/features/generic_file_show_spec.rb
+++ spec/features/generic_file_show_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "User Generic File" do
+describe "Showing the Generic File" do
   let(:user_name) {"curator1"}
 
   before do
@@ -16,12 +16,12 @@ describe "User Generic File" do
       @gf1.apply_depositor_metadata(@user.user_key)
       @gf1.contributor = "Mohammad"
       @gf1.creator = "Allah"
-      @gf1.description = "The work by Allah"
+      @gf1.description = "The work by Allah http://example.org/TheDescriptionLink/"
       @gf1.publisher = "Vertigo Comics"
       @gf1.subject = "Theology"
       @gf1.language = "Arabic"
       @gf1.based_near = "Medina, Saudi Arabia"
-      @gf1.related_url = "http://example.org/TheWork/"
+      @gf1.related_url = "http://example.org/TheRelatedURLLink/"
       @gf1.save!
       visit "/"
       click_link @gf1.title.first
@@ -35,8 +35,15 @@ describe "User Generic File" do
       page.should have_content @gf1.title.first
     end
 
-    it "displays a link for 'Related URL'" do
-      #save_and_open_page
+    it "displays a link for all the linkable items" do
+       save_and_open_page
+      page.should have_link @gf1.creator.first
+      page.should have_link @gf1.contributor.first
+      page.should have_link @gf1.description.first
+      page.should have_link @gf1.publisher.first
+      page.should have_link @gf1.subject.first
+      page.should have_link @gf1.language.first
+      page.should have_link @gf1.based_near.first 
       page.should have_link @gf1.related_url.first
     end
   end
