93d24b9c2bb01dae98f4546540eb50093b950c7e
Carolyn Cole and Chet Swalina <developers+carolyn+chet@westarete.com>
2014-01-30 11:17:17 -0500

    Failing feature test for Related URL not being a link refs #9014

---
+++ spec/features/user_generic_file_show_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "User Generic File" do
+  let(:user_name) {"curator1"}
+
+  before do
+    # TODO: This really shouldn't be necessary
+    unspoof_http_auth
+    sign_in :curator
+    @user = User.where(login: 'curator1').first
+
+  end
+  context "User with generic files" do
+    before do
+      @gf1 =  GenericFile.new title: 'file title', resource_type: 'Video'
+      @gf1.apply_depositor_metadata(@user.user_key)
+      @gf1.contributor = "Mohammad"
+      @gf1.creator = "Allah"
+      @gf1.description = "The work by Allah"
+      @gf1.publisher = "Vertigo Comics"
+      @gf1.subject = "Theology"
+      @gf1.language = "Arabic"
+      @gf1.based_near = "Medina, Saudi Arabia"
+      @gf1.related_url = "http://example.org/TheWork/"
+      @gf1.save!
+      visit "/"
+    end
+    after do
+      @gf1.destroy  rescue puts "error occured destroying object"
+    end
+
+    it "loads the page" do
+      click_link @gf1.title.first
+     #save_and_open_page
+      page.status_code.should == 200
+      page.should have_content @gf1.title.first
+    end
+
+    it "displays a link for 'Related URL'" do
+      page.should have_link "http://example.org/TheWork/"
+    end
+  end
+end
