256f619092bae736559f3b4968a75214efc774a7
Carolyn Cole <cam156@psu.edu>
2014-02-12 12:55:18 -0500

    adding vframe to video thumbnail creation so only one frame gets captured

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -75,6 +75,22 @@ class GenericFile < ActiveFedora::Base
      return stat
    end
 
+  def create_video_thumbnail
+    return unless Sufia.config.enable_ffmpeg
+
+    output_file = Dir::Tmpname.create(['sufia', ".png"], Sufia.config.temp_file_base){}
+    content.to_tempfile do |f|
+      # we could use something like this in order to find a frame in the middle.
+      #ffprobe -show_files video.avi 2> /dev/null | grep duration | cut -d= -f2 53.399999
+      command = "#{Sufia.config.ffmpeg_path} -i \"#{f.path}\" -loglevel quiet -vf \"scale=338:-1\"  -r  1  -t  1 -vframes 1 #{output_file}"
+      system(command)
+      raise "Unable to execute command \"#{command}\"" unless $?.success?
+    end
+
+    self.thumbnail.content = File.open(output_file, 'rb').read
+    self.thumbnail.mimeType = 'image/png'
+  end
+
 
   def extract_content
     begin
