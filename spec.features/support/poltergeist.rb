6a224f147363295c8b3857d2ea023a7647c44faa
Chet Swalina, Luke Niedermyer, and Scott Woods <developers+chet+luke+scott@westarete.com>
2014-03-18 13:44:01 -0400

    Clear entire mysql database between each test run
    This is going to be slower, but it's important for consistency and test
    independence.

--- spec.features/support/poltergeist.rb
+++ spec.features/support/poltergeist.rb
@@ -10,8 +10,19 @@ end
 Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
 
-# Phantomjs runs in a separate process so our transactions will not be visible
-# to it; we need to commit all our changes to the database.
+# Configure how we should clear the database after each test.
+# In the long run, we may want to move this out of the poltergeist setup,
+# but for now, our database cleaning strategy is relevant to the fact that
+# we are using poltergeist.
 RSpec.configure do |config|
+
+  # Phantomjs runs in a separate process so our transactions will not be visible
+  # to it; we need to commit all our changes to the database.
   config.use_transactional_fixtures = false
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
+
 end
