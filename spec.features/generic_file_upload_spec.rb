c3f8959b6afc15981c1209f5c8039c885591ea58
Chet Swalina <cswalina@gmail.com>
2014-03-27 13:42:59 -0400

    WIP: Generic file upload feature spec

---
+++ spec.features/generic_file_upload_spec.rb
@@ -0,0 +1,34 @@
+require_relative './feature_spec_helper'
+
+describe "Generic File uploading and downloading", request: true do
+
+  after(:all) do
+    User.destroy_all
+    Batch.destroy_all
+  end
+
+  context "when logged in as a curator" do
+    let(:current_user) { FactoryGirl.create(:user) }
+    let(:filename) { 'world.png' }
+
+    before do
+      sign_in_as current_user
+    end
+
+    it "Uploads successfully" do
+      visit new_generic_file_path
+      check "terms_of_service"
+      test_file_path = Rails.root.join("spec/fixtures/#{filename}").to_s
+      page.execute_script(%Q{$("input[type=file]").css("opacity", "1").css("-moz-transform", "none");$("input[type=file]").attr('id',"fileselect");})
+      attach_file("fileselect", test_file_path)
+      click_button 'main_upload_start'
+      page.should have_content 'Apply Metadata'
+      fill_in 'generic_file_tag', with: 'test_generic_file_tag'
+      fill_in 'generic_file_creator', with: 'test_generic_file_creator'
+      select 'Attribution-NonCommercial-NoDerivs 3.0 United States', from: 'generic_file_rights'
+      click_on 'upload_submit'
+      page.should have_content 'My Dashboard'
+      page.should have_content filename
+    end
+  end
+end
