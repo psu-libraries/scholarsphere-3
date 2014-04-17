da208125d31df4e00ed4669553d02165e726a15c
Chet Swalina <cswalina@gmail.com>
2014-04-17 13:46:21 -0400

    Use helper method to test generic file upload

--- spec.features/generic_file_upload_spec.rb
+++ spec.features/generic_file_upload_spec.rb
@@ -11,15 +11,7 @@ describe "Generic File uploading and downloading:" do
     end
 
     specify "I can upload a file successfully" do
-      visit new_generic_file_path
-      check "terms_of_service"
-      attach_file("files[]", test_file_path(filename))
-      click_button 'main_upload_start'
-      page.should have_content 'Apply Metadata'
-      fill_in 'generic_file_tag', with: 'test_generic_file_tag'
-      fill_in 'generic_file_creator', with: 'test_generic_file_creator'
-      select 'Attribution-NonCommercial-NoDerivs 3.0 United States', from: 'generic_file_rights'
-      click_on 'upload_submit'
+      upload_generic_file(filename)
       page.should have_content 'My Dashboard'
       page.should have_content filename
     end
