05407c06b6e6edba2eee2d9a2ad105c1bbbcdcd7
Chet Swalina and Luke Niedermyer <developers+chet+luke@westarete.com>
2014-05-01 17:10:36 -0400

    WIP: Rough out the dashboard spec

--- spec.features/wip_dashboard_spec.rb
+++ spec.features/wip_dashboard_spec.rb
@@ -1,75 +1,85 @@
 # This file is a Work in Progress
 
-
 require_relative './feature_spec_helper'
 
 describe 'Dashboard:' do
 
-  describe 'view_dashboard' do
+  let(:current_user) { create :user }
+  let(:title) { 'Test Collection Title' }
+  let(:creator) { 'Test Creator Name' }
+  let(:description) { 'Description for our test collection.' }
 
-    let(:current_user) { create :user }
+  # before do
+  #   sign_in_as current_user
+  #   upload_generic_file 'world.png'
+  #   check 'check_all'
+  #   click_button 'Add to Collection'
+  #   db_create_populated_collection_button.click
+  #   create_collection title, creator, description
+  # end
 
-    def create_collection
+  describe 'For an item in my list' do
+    specify 'Clicking the Visibility link loads the edit permissions page'
+    specify 'Clicking + displays additional metadata about that file'
+    specify 'Clicking Edit File goes directly to the metadata edit page'
+    specify 'Clicking Download File downloads the file'
+    specify 'Clicking Delete File removes the file'
+    specify 'Clicking Highlight File will highlight that file on my profile'
+    specify 'Clicking Transfer Ownership of File loads the transfer ownership page'
+    describe 'The Single-Use Link:' do
+      pending 'Places the link on the clipboard'
+      pending 'The first visit displays the file data'
+      pending 'Subsequent visits fail to load the page'
     end
-    before do
-      sign_in_as current_user
-      upload_generic_file 'world.png'
-      create_collection
+  end
+
+  describe 'Pagination:' do
+    context 'Given I have uploaded 15 files' do
+      specify 'The files are listed on 2 pages'
+      specify 'Changing Show per page to 20 lists all the files on one page'
     end
+  end
 
-    let (:gf1) { GenericFile.last }
+  describe 'Search:' do
+    specify 'Searching by partial titles does not display any results'
+    specify 'Searching with exact words displays the correct results'
+    specify 'Searching by Resource Type displays the correct results'
+    specify 'Searching by Keywords displays the correct results'
+    specify 'Searching by Creator displays the correct results'
+  end
 
-    before (:all) do
-      user_key = 'jilluser'
-      @collection = Collection.new title: 'collection title'
-      @collection.description = 'collection description'
-      @collection.apply_depositor_metadata user_key
-      @collection.save!
-      @gf1 =  GenericFile.new title: 'file title', resource_type: 'Video'
-      @gf1.apply_depositor_metadata user_key
-      @gf1.save!
-    end
+  describe 'Facets:' do
+    specify 'Displays the correct totals for each type'
+  end
 
+  describe 'Sorting:' do
+    specify 'Items are sorted correctly'
+  end
 
-    describe 'visit dashboard' do
-
-      it 'should visit dashboard' do
-        go_to_dashboard
-        page.should have_content 'My Dashboard'
-        page.should have_content @gf1.title.first
-        page.should have_content @collection.title
-        page.should have_content @collection.description
-      end
-
-      it 'shows image thumbnail' do
-        allow_any_instance_of(SolrDocument).to receive(:image?).and_return(true)
-        go_to_dashboard
-        page.should have_css("img[src*='#{Sufia::Engine.routes.url_helpers.download_path(@gf1.noid, {datastream_id: 'thumbnail'})}']")
-      end
-      it 'shows pdf thumbnail' do
-        allow_any_instance_of(SolrDocument).to receive(:pdf?).and_return(true)
-        go_to_dashboard
-        page.should have_css("img[src*='#{Sufia::Engine.routes.url_helpers.download_path(@gf1.noid, {datastream_id: 'thumbnail'})}']")
-      end
-      it 'shows video thumbnail' do
-        allow_any_instance_of(SolrDocument).to receive(:video?).and_return(true)
-        go_to_dashboard
-        page.should have_css("img[src*='#{Sufia::Engine.routes.url_helpers.download_path(@gf1.noid, {datastream_id: 'thumbnail'})}']")
-      end
-      it 'shows audio thumbnail' do
-        allow_any_instance_of(SolrDocument).to receive(:audio?).and_return(true)
-        go_to_dashboard
-        page.should have_css('img[src*="/assets/audio.png"]')
-      end
-      it 'shows default thumbnail' do
-        go_to_dashboard
-        page.should have_css('img[src*="/assets/default.png"]')
-      end
-    end
-
-    def go_to_dashboard
-      visit '/'
-      click_link 'my dashboard'
-    end
+  describe 'Thumbnails:' do
+    it 'shows image thumbnail'
+   #  allow_any_instance_of(SolrDocument).to receive(:image?).and_return(true)
+   #  go_to_dashboard
+   #  page.should have_css("img[src*='#{Sufia::Engine.routes.url_helpers.download_path(@gf1.noid, {datastream_id: 'thumbnail'})}']")
+   #end
+    it 'shows pdf thumbnail'
+   #  allow_any_instance_of(SolrDocument).to receive(:pdf?).and_return(true)
+   #  go_to_dashboard
+   #  page.should have_css("img[src*='#{Sufia::Engine.routes.url_helpers.download_path(@gf1.noid, {datastream_id: 'thumbnail'})}']")
+   #end
+    it 'shows video thumbnail'
+   #  allow_any_instance_of(SolrDocument).to receive(:video?).and_return(true)
+   #  go_to_dashboard
+   #  page.should have_css("img[src*='#{Sufia::Engine.routes.url_helpers.download_path(@gf1.noid, {datastream_id: 'thumbnail'})}']")
+   #end
+    it 'shows audio thumbnail'
+   #  allow_any_instance_of(SolrDocument).to receive(:audio?).and_return(true)
+   #  go_to_dashboard
+   #  page.should have_css('img[src*="/assets/audio.png"]')
+   #end
+    it 'shows default thumbnail'
+   #  go_to_dashboard
+   #  page.should have_css('img[src*="/assets/default.png"]')
+   #end
   end
 end
