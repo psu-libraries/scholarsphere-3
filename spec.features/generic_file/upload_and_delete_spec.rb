36c7008e3e373ed489d5296997bbdd1fe320ec50
Chet Swalina and Luke Niedermyer <developers+chet+luke@westarete.com>
2014-05-05 17:08:38 -0400

    Update generic file spec structure

--- spec.features/generic_file/upload_and_delete_spec.rb
+++ spec.features/generic_file/upload_and_delete_spec.rb
@@ -1,29 +1,29 @@
 require_relative '../feature_spec_helper'
 
-describe 'Generic File uploading and downloading:' do
+include Selectors::Dashboard
+
+describe 'Generic File uploading and deletion:' do
 
   context 'When logged in as a PSU user' do
     let(:current_user) { create :user }
     let(:filename) { 'world.png' }
+    let(:file) { GenericFile.find(Solrizer.solr_name("desc_metadata__title")=>"world.png").first }
 
     before do
       sign_in_as current_user
+      upload_generic_file filename
     end
 
     specify 'I can upload a file successfully' do
-      upload_generic_file filename
       page.should have_content 'My Dashboard'
       page.should have_content filename
     end
 
-    pending 'I can download an uploaded file' do
-      # db_item_actions_toggle(file).click
-      # click_link 'Download File'
-      # p ">>> #{page.response_headers.inspect}"
-    end
-
-    pending 'I can delete an uploaded file' do
-
+    specify 'I can delete an uploaded file' do
+      page.should have_content file.title.first
+      db_item_actions_toggle(file).click
+      click_link 'Delete File'
+      page.should_not have_content file.title.first
     end
   end
 
@@ -34,7 +34,7 @@ describe 'Generic File uploading and downloading:' do
       sign_in_as current_user
     end
 
-    specify 'I cannot get to the upload page' do
+    specify 'I cannot access the upload page' do
       visit new_generic_file_path
       page.should have_content 'Unauthorized'
       page.should_not have_content 'Upload'
