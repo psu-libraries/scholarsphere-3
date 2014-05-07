d9fd19b5ce962929adda840c3fb52667954dc817
Paul H. Crum <pcrum@paul.crum-scholarsphere>
2014-05-07 03:26:36 -0400

    Removed permission notification behavior out of the generic file controller, moved it into the generic file model.

--- app/controllers/generic_files_controller.rb
+++ app/controllers/generic_files_controller.rb
@@ -1,8 +1,7 @@
 class GenericFilesController < ApplicationController
-  require_relative '../../app/controllers/concerns/permission_notification'
   include Sufia::Controller
   include Sufia::FilesControllerBehavior
-  include Scholarsphere::PermissionNotification
+
 
   # Overriding Sufia::FilesControllerBehavior to save on_behalf_of
   def update_metadata_from_upload_screen
---
+++ app/models/concerns/permission_notification_behavior.rb
@@ -0,0 +1,89 @@
+module Scholarsphere
+  module PermissionNotification
+    extend ActiveSupport::Concern
+
+    included do
+      after_initialize :store_current_permissions
+      after_update :evaluate_permission_deltas
+    end #end included
+
+    # stores the current permissions of the generic file in a variable
+    def store_current_permissions
+      @previous_permissions ||= permissions
+    end #end store_previous_permissions
+
+    # evaluates permission deltas and notifies users of access
+    def evaluate_permission_deltas
+      previous_permissions = @previous_permissions
+      current_permissions = permissions
+      permission_state = calculate_permission_state(previous_permissions,current_permissions)
+      notify_users_of_access(permission_state)
+    end #end evaluate_permission_deltas
+
+    #generates a hash of the file permission state including added, deleted, and modified permissions
+    #format { added:  <hash of added permissions : name  => { name,access,type }
+    #         deleted: <hash of deleted permissions : name => {name,access,type}
+    #         modified: <hash of modified permissions : name => {name,access,type,previous_access}
+    #         unchanged: <hash of unchanged permissions : name => {name,access,type}
+    # }
+    def calculate_permission_state(previous_permissions,current_permissions)
+      state = {}
+      chash = {}
+      phash = {}
+
+      #converts current permissions to key value hash where user id is hash
+      chash = current_permissions.inject({}){|result,el| result[el[:name]]=el; result} unless current_permissions.nil?
+
+      #converts previous permissions to key value hash where user id is hash
+      phash = previous_permissions.inject({}){|result,el| result[el[:name]]=el; result} unless previous_permissions.nil?
+
+      #append added permissions to the state hash
+      state[:added] = {}
+      (chash.keys - phash.keys).each{ |key| state[:added][key] = chash[key].deep_dup }
+
+      #append removed permissions to the state hash
+      state[:removed] = {}
+      (phash.keys - chash.keys).each{ |key| state[:removed][key] = phash[key].deep_dup }
+
+      #append modified or unchanged permissions to the state hash
+      state[:modified] = {}
+      state[:unchanged] = {}
+      (phash.keys & chash.keys).each do |key|
+        if phash[key].eql?(chash[key])
+          state[:unchanged][key] = phash[key].deep_dup
+        else
+          state[:modified][key] = phash[key].deep_dup
+          state[:modified][key][:previous_access] = chash[key][:access]
+        end
+      end
+
+      return state
+    end #end calculate_permission_state
+
+    # retrieve user/user(s) who should be notified and send notification
+    def notify_users_of_access(permission_state)
+      permission_state[:added].each do |entityid,entry|
+        if entry[:type]=="user"
+          user = User.find_by_user_key(entityid)
+          send_notification(user,entry[:access]) unless user.nil? || user.login == User.current.login
+        else
+          User.users_of_group(entityid).each do |user|
+            send_notification(user,entry[:access]) unless user.login == User.current.login
+          end
+        end
+      end
+    end #end notify_users_of_access
+
+    # send user notification
+    def send_notification(recipient,access)
+      sender = User.batchuser
+      sender.send_message(recipient,
+                          I18n.t('scholarsphere.notifications.permissions.notification',
+                                 user: User.current.login,
+                                 file: title[0],
+                                 access: access == 'read'? 'View/Download' : 'Edit'),
+                          I18n.t('scholarsphere.notifications.permissions.subject')) unless recipient.nil?
+    end #end send_notification
+
+  end #end PermissionNotification
+end #end Scholarsphere
