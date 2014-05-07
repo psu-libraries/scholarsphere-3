7721a9358f2633a198a925eeded7a4faf1c77b3d
Chet Swalina and Luke Niedermyer <developers+chet+luke@westarete.com>
2014-05-07 11:49:05 -0400

    Update create_file default values

--- spec.features/support/helpers/generic_files.rb
+++ spec.features/support/helpers/generic_files.rb
@@ -15,14 +15,14 @@ module GenericFilesHelper
 
   def create_file (user, options={})
     GenericFile.new.tap do |f|
-      f.title         = options[:title] || 'new title'
+      f.title         = options[:title] || 'Title'
       f.resource_type = options[:resource_type] || 'Video'
-      f.creator       = options[:creator] || 'Creator1'
-      f.tag           = options[:tag] || 'Keyword1'
-      f.subject       = options[:subject] || 'Subject1'
-      f.language      = options[:language] || 'Language1'
-      f.based_near    = options[:based_near] || 'Location1'
-      f.publisher     = options[:publisher] || 'Publisher1'
+      f.creator       = options[:creator] || 'Creator'
+      f.tag           = options[:tag] || 'Keyword'
+      f.subject       = options[:subject] || 'Subject'
+      f.language      = options[:language] || 'Language'
+      f.based_near    = options[:based_near] || 'Location'
+      f.publisher     = options[:publisher] || 'Publisher'
       f.rights        = options[:rights] || 'http://creativecommons.org/licenses/by-nc-nd/3.0/us/'
       f.read_groups   = options[:read_groups] || ['public']
       f.apply_depositor_metadata(user.login)
