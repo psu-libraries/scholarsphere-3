e1053c0d6534b9de8a59501a82658789e4aacb0d
Carolyn Cole <cam156@psu.edu>
2014-07-28 08:35:40 -0400

    Stream lining dashboard feature test to not have as many startups.  This is an effort to make the test more stable and faster

--- spec/features/dashboard/dashboard_files_spec.rb
+++ spec/features/dashboard/dashboard_files_spec.rb
@@ -6,50 +6,73 @@ describe 'Dashboard Files' do
 
   let!(:current_user) { create :user }
 
+  let!(:file) { create_file current_user, { title: 'little_file.txt', creator: 'little_file.txt_creator', resource_type: "stuff" } }
+
   before do
     sign_in_as current_user
-    upload_generic_file 'little_file.txt'
+    go_to_dashboard_files
   end
 
-  let(:file) { GenericFile.find(Solrizer.solr_name("desc_metadata__title")=>"little_file.txt").first }
-  let(:filename) { file.filename.first }
+  let(:filename) { file.title.first }
 
   context 'with one file:' do
 
-    specify 'tab title and buttons' do
+    specify 'interactions are wired correctly' do
+      #tab title and buttons
       page.should have_content("My Files")
       within('.col-xs-12.col-sm-3') do
         page.should have_content("Upload")
         page.should have_content("Create Collection")
       end
-    end
+      # Additional metadata about the file is hidden
+      page.should_not have_content "Edit Access"
+      page.should_not have_content "little_file.txt_creator"
 
-    specify 'Clicking the Visibility link loads the edit permissions page' do
+      # Clicking + displays additional metadata about that file
+      within("#documents") do
+        first('i.glyphicon-chevron-right').click
+      end
+      page.should have_content "little_file.txt_creator"
+      page.should have_content "Edit Access"
+
+      db_item_actions_toggle(file).click
+      click_link 'Edit File'
+      page.should have_content "Edit #{filename}"
+
+      # Clicking the Visibility link loads the edit permissions page
       # The link is not visible in poltergeist unless we resize
       # the page (1440w x 1200h). Somehow using .trigger gets around
       # this issue though.
+      go_to_dashboard_files
       db_visibility_link(file).trigger('click')
       page.should have_content 'Permissions'
       page.should have_content 'Visibility'
       page.should have_content 'Share With'
-    end
 
-    specify 'Additional metadata about the file is hidden' do
-      page.should_not have_content "plain (Plain text)JPG"
-      page.should_not have_content "little_file.txt_creator"
-    end
-
-    specify 'Clicking + displays additional metadata about that file' do
-      first('i.glyphicon-chevron-right').click
-      page.should have_content "plain (Plain text)JPG"
-      page.should have_content "little_file.txt_creator"
-    end
-
-    specify 'Clicking Edit File goes directly to the metadata edit page' do
+      # Clicking Transfer Ownership of File loads the transfer ownership page
+      go_to_dashboard_files
       db_item_actions_toggle(file).click
-      click_link 'Edit File'
-      page.should have_content "Edit #{filename}"
+      click_link 'Transfer Ownership of File'
+      page.should have_content "Transfer ownership of \"#{filename}\""
     end
+
+    #specify 'Clicking the Visibility link loads the edit permissions page' do
+    #end
+
+    #specify 'Additional metadata about the file is hidden' do
+    #end
+
+    #specify 'Clicking + displays additional metadata about that file' do
+    #  first('i.glyphicon-chevron-right').click
+    #  page.should have_content "plain (Plain text)JPG"
+    #  page.should have_content "little_file.txt_creator"
+    #end
+
+    #specify 'Clicking Edit File goes directly to the metadata edit page' do
+    #  db_item_actions_toggle(file).click
+    #  click_link 'Edit File'
+    #  page.should have_content "Edit #{filename}"
+    #end
 
     context 'When I highlight it' do
       before do
@@ -59,27 +82,30 @@ describe 'Dashboard Files' do
         page.should have_content "Unhighlight File"
         db_item_actions_toggle(file).trigger('click')
       end
-      specify 'It is highlighted on my profile' do
+      specify 'It is highlighted' do
+        #It is highlighted on my profile
         visit "/users/#{current_user.login}"
         page.should have_css '.active a', text:"Contributions"
         within '#contributions' do
           page.should have_link "#{file.filename.first}"
         end
-      end
 
-      specify 'It is displayed on my highlights' do
+        #It is displayed on my highlights
         go_to_dashboard_highlights
         within '#documents' do
           page.should have_link "#{file.filename.first}"
         end
       end
+
+      #specify 'It is displayed on my highlights' do
+      #end
     end
 
-    specify 'Clicking Transfer Ownership of File loads the transfer ownership page' do
-      db_item_actions_toggle(file).click
-      click_link 'Transfer Ownership of File'
-      page.should have_content "Transfer ownership of \"#{filename}\""
-    end
+    #specify 'Clicking Transfer Ownership of File loads the transfer ownership page' do
+    #  db_item_actions_toggle(file).click
+    #  click_link 'Transfer Ownership of File'
+    #  page.should have_content "Transfer ownership of \"#{filename}\""
+    #end
 
     describe 'The Single-Use Link:' do
       pending 'Places the link on the clipboard'
@@ -144,13 +170,13 @@ describe 'Dashboard Files' do
 
       context 'When I search by Keywords' do
         before do
-          search_my_files_by_term( file.tag )
-          page.should have_content "You searched for: #{file.tag}"
+          search_my_files_by_term( file.tag.first )
+          page.should have_content "You searched for: #{file.tag.first}"
         end
         it 'Displays the correct results' do
           page_should_only_list file
-        end
-        it 'allows me to remove constraints' do
+
+          # allows me to remove constraints
           find('span.glyphicon-remove').click
           page.should_not have_content "You searched for:"
         end
@@ -168,17 +194,17 @@ describe 'Dashboard Files' do
     end
 
     describe 'Facets:' do
-      {
-        'Resource Type' => 'Video (10)',
-        'Creator'       => 'Creator1 (10)',
-        'Keyword'       => 'Keyword1 (10)',
-        'Subject'       => 'Subject1 (10)',
-        'Language'      => 'Language1 (10)',
-        'Location'      => 'Location1 (10)',
-        'Publisher'     => 'Publisher1 (10)',
-        'File Format'   => 'plain () (10)'
-      }.each do |facet, value|
-        specify "Displays the correct totals for #{facet}" do
+      specify "Displays the correct totals for facet" do
+        {
+          'Resource Type' => 'Video (10)',
+          'Creator'       => 'Creator1 (10)',
+          'Keyword'       => 'Keyword1 (10)',
+          'Subject'       => 'Subject1 (10)',
+          'Language'      => 'Language1 (10)',
+          'Location'      => 'Location1 (10)',
+          'Publisher'     => 'Publisher1 (10)',
+          'File Format'   => 'plain () (10)'
+        }.each do |facet, value|
           within("#facets") do
             click_link(facet)
             page.should have_content(value)
@@ -191,10 +217,10 @@ describe 'Dashboard Files' do
       specify 'Items are sorted correctly' do
         find('#sort').find(:xpath, 'option[4]').select_option
         click_button("Refresh")
-        page.should_not have_content(GenericFile.all.last.title.first)
+        page.should have_content(file.title.first)
         find('#sort').find(:xpath, 'option[5]').select_option
         click_button("Refresh")
-        page.should have_content(GenericFile.all.last.title.first)
+        page.should have_content(file.title.first)
       end
     end
   end
@@ -232,15 +258,15 @@ describe 'Dashboard Files' do
   def create_files(user, number_of_files)
     (1..number_of_files).each do |x|
       GenericFile.new.tap do |f|
-        f.title = "title_#{x}"
+        f.title = ["title_#{x}"]
         f.apply_depositor_metadata(user.login)
-        f.resource_type = 'Video'
-        f.creator = 'Creator1'
-        f.tag = 'Keyword1'
-        f.subject = 'Subject1'
-        f.language = 'Language1'
-        f.based_near = 'Location1'
-        f.publisher = 'Publisher1'
+        f.resource_type = ['Video']
+        f.creator = ['Creator1']
+        f.tag = ['Keyword1']
+        f.subject = ['Subject1']
+        f.language = ['Language1']
+        f.based_near = ['Location1']
+        f.publisher = ['Publisher1']
         f.mime_type = 'text/plain'
         f.save!
       end
--- spec/support/helpers/locations.rb
+++ spec/support/helpers/locations.rb
@@ -6,26 +6,30 @@ module Locations
   end
 
   def go_to_dashboard_files
-    go_to_dashboard
-    click_link('View Files')
+    #go_to_dashboard
+    #click_link('View Files')
+    visit '/dashboard/files'
     expect(page).to have_selector('li.active', text:"My Files")
   end
 
   def go_to_dashboard_collections
-    go_to_dashboard_files
-    click_link('My Collections')
+    #go_to_dashboard_files
+    #click_link('My Collections')
+    visit '/dashboard/collections'
     page.should have_content('My Collections')
   end
 
   def go_to_dashboard_shares
-    go_to_dashboard_files
-    click_link('Files Shared with Me')
+    #go_to_dashboard_files
+    #click_link('Files Shared with Me')
+    visit '/dashboard/shares'
     page.should have_content('Files Shared with Me')
   end
 
   def go_to_dashboard_highlights
-    go_to_dashboard_files
-    click_link('My Highlights')
+    #go_to_dashboard_files
+    #click_link('My Highlights')
+    visit '/dashboard/highlights'
     page.should have_content('My Highlights')
   end
 
