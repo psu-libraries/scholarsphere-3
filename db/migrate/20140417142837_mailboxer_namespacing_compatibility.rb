f4e0182968096d138dbeb0d0d7c7f1d8383f2fa5
Carolyn Cole <cam156@psu.edu>
2014-07-31 08:10:05 -0400

    Fixing the down on the migration so it does not cause an error if it is restarted

--- db/migrate/20140417142837_mailboxer_namespacing_compatibility.rb
+++ db/migrate/20140417142837_mailboxer_namespacing_compatibility.rb
@@ -19,14 +19,18 @@ class MailboxerNamespacingCompatibility < ActiveRecord::Migration
   end
 
   def self.down
-    remove_index "mailboxer_receipts","notification_id"
-    remove_index "mailboxer_notifications","conversation_id"
+    remove_foreign_key "mailboxer_receipts", name: "mailboxer_receipts_on_notification_id_#{Rails.env}"
+    #Messages
+    remove_foreign_key "mailboxer_notifications", name: "notifications_on_conversation_id_#{Rails.env}"
 
     rename_table :mailboxer_conversations, :conversations
     rename_table :mailboxer_notifications, :notifications
     rename_table :mailboxer_receipts,      :receipts
 
-    remove_index "receipts","notification_id"
-    remove_index "notifications","conversation_id"
+    add_foreign_key "receipts", "notifications", name:"receipts_on_notification_id_#{Rails.env}", column: :notification_id
+    #Messages
+    add_foreign_key "notifications", "conversations", name:"notifications_on_conversation_id_#{Rails.env}",column:"conversation_id"
+    #
+
   end
 end
