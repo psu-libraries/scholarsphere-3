d2b676e1b85802461724befbcf47a28c0a89e9fe
Carolyn Cole <cam156@psu.edu>
2014-08-18 09:38:04 -0400

    Adding test for file listing heading.  other fiexs were in projecthydra/sufia/643.  fixes #9508

--- spec/features/dashboard/dashboard_files_spec.rb
+++ spec/features/dashboard/dashboard_files_spec.rb
@@ -20,6 +20,7 @@ describe 'Dashboard Files' do
     specify 'interactions are wired correctly' do
       #tab title and buttons
       expect(page).to have_content("My Files")
+      expect(page).to have_selector("h1.sr-only", text: "Files listing")
       within('#sidebar') do
         expect(page).to have_content("Upload")
         expect(page).to have_content("Create Collection")
