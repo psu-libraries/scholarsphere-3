be41df4266074734cfc1754375a54a2cc54b4c91
Adam Wead <amsterdamos@gmail.com>
2014-08-20 16:20:41 -0400

    Sufia 4.0.0 and updated gems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,58 +15,18 @@
 class UsersController < ApplicationController
   include Sufia::UsersControllerBehavior
 
-  # Display user profile
-  def show
-    @events = @user.profile_events(100) rescue []
-    @followers = @user.followers
-    @following = @user.all_following
+  before_filter :get_linkedin_url, only: :show
+
+  def get_linkedin_url
     @linkedInUrl = @user.linkedin_handle
     @linkedInUrl = "http://www.linkedin.com/in/" + @linkedInUrl unless @linkedInUrl.blank? or @linkedInUrl.include? 'linkedin.com'
     @linkedInUrl = "http://"+ @linkedInUrl unless @linkedInUrl.blank? or @linkedInUrl.include? 'http'
-    adjust_trophies!
   end
 
-  # Display form for users to edit their profile information
-  def edit
-    @user = current_user
-    @groups = @user.groups
-    @trophies = []
-    adjust_trophies!
-  end
-
-
-  def index
-    sort_val = get_sort
-    query = params[:uq].blank? ? nil : "%"+params[:uq].downcase+"%"
-    base = User.where(*base_query)
-    unless query.blank?
-      base = base.where("#{Devise.authentication_keys.first} like lower(?) OR lower( display_name) like lower(?)", query, query)
-    end
-    @users = base.order(sort_val).page(params[:page]).per(10)
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @users.to_json }
-    end
-
-  end
-    protected
-
-  def adjust_trophies!
-    @user.trophies.reject!  do  |t|
-      begin
-        GenericFile.load_instance_from_solr(Sufia::Noid.namespaceize(t.generic_file_id))
-        false
-      rescue ActiveFedora::ObjectNotFoundError
-        t.destroy
-        true
-      end
-    end
-    @user.reload
-    @trophies = @user.trophy_files
-  end
+  protected
 
   def base_query
     ["ldap_available = ? AND login not in ('testapp','tstem31')", true]
   end
+
 end
