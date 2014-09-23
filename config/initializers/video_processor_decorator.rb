3ad5ba7ef429e617c5c325c5e4163a67275827af
Carolyn Cole <cam156@psu.edu>
2014-09-23 11:52:22 -0400

    Adding a timeout to the derivative executable running so soffice does not hang.  fixes #9607

--- config/initializers/video_processor_decorator.rb
+++ config/initializers/video_processor_decorator.rb
@@ -16,3 +16,26 @@ Hydra::Derivatives::Video.class_eval do
   end
 
 end
+Hydra::Derivatives::ShellBasedProcessor::ClassMethods.class_eval do
+  alias_method :old_execute, :execute
+  def execute(command)
+    puts "got to override"
+    timeout = 5*60 # 5 minutes
+    wait_thr = nil
+    begin
+      status = Timeout::timeout(timeout) do
+        stdin, stdout, stderr, wait_thr = popen3(command)
+        stdin.close
+        out = stdout.read
+        stdout.close
+        err = stderr.read
+        stderr.close
+        raise "Unable to execute command \"#{command}\"\n#{err}" unless wait_thr.value.success?
+      end
+    rescue Timeout::Error => ex
+      pid = wait_thr[:pid]
+      Process.kill("KILL", pid)
+      raise "Unable to execute command \"#{command}\"\nThe command took longer than #{timeout} to execute"
+    end
+  end
+end
