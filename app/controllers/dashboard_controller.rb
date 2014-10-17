ccd85204a582e9cf5164ee27bf693e7daef66053
Carolyn Cole <cam156@psu.edu>
2014-10-17 13:04:41 -0400

    removing scholarsphere override for user activity now that sufia does it correctly.  fixes #9584

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,13 +6,4 @@ class DashboardController < ApplicationController
     @outgoing = ProxyDepositRequest.where(sending_user_id: current_user.id)
     super
   end
-
-  protected
-
-  def gather_dashboard_information
-    @user = current_user
-    @activity = current_user.get_all_user_activity(params[:since].blank? ? DateTime.now.to_i - 24*60*60 : params[:since].to_i)
-    @notifications = current_user.mailbox.inbox
-  end
-
 end
