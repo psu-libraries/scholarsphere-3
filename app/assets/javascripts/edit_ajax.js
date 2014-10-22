4b6631993625a08157941a5c17a51bbcd032d884
Carolyn Cole <cam156@psu.edu>
2014-10-22 15:28:40 -0400

    Updating the stats page to use GenericFile.where. fixes #9642

--- app/assets/javascripts/edit_ajax.js
+++ app/assets/javascripts/edit_ajax.js
@@ -2,37 +2,38 @@
 //  This override should be removed once sufia changes to add loading onto detail_ instead of collapse_ here:
 // https://github.com/projecthydra/sufia/blob/master/app/assets/javascripts/sufia/batch_edit.js#L107 & https://github.com/projecthydra/sufia/blob/master/app/assets/javascripts/sufia/batch_edit.js#L117
 
-$( document ).ajaxSend(function( event, jqxhr, settings ) {
-    form_id = settings["form"];
-    if ((form_id) && (typeof(myVar) != 'undefined')) {
-        var key = form_id.replace("form_", "");
-        if (key) {
-            var outer_div = "#detail_" + key;
-            $(outer_div).addClass("loading");
-        }
-    }
-});
-
-$( document ).ajaxSuccess(function( event, jqxhr, settings ) {
-    form = settings["form"];
-    form_id = settings["form"];
-    if ((form_id) && (typeof(myVar) != 'undefined')) {
-        var key = form_id.replace("form_", "");
-        if (key) {
-            var outer_div = "#detail_" + key;
-            $(outer_div).removeClass("loading");
-        }
-    }
-});
-
-$( document ).ajaxError(function( event, jqxhr, settings ) {
-    form = settings["form"];
-    form_id = settings["form"];
-    if (form_id) {
-        var key = form_id.replace("form_", "");
-        if (key) {
-            var outer_div = "#detail_" + key;
-            $(outer_div).removeClass("loading");
-        }
-    }
-});
+//$( document ).ajaxSend(function( event, jqxhr, settings ) {
+//    form_id = settings["form"];
+//    if ((form_id) && (typeof(myVar) != 'undefined')) {
+//        var key = form_id.replace("form_", "");
+//        if (key) {
+//            var outer_div = "#detail_" + key;
+//            $(outer_div).addClass("loading");
+//        }
+//    }
+//});
+//
+//$( document ).ajaxSuccess(function( event, jqxhr, settings ) {
+//    form = settings["form"];
+//    form_id = settings["form"];
+//    if ((form_id) && (typeof(myVar) != 'undefined')) {
+//        var key = form_id.replace("form_", "");
+//        if (key) {
+//            var outer_div = "#detail_" + key;
+//            $(outer_div).removeClass("loading");
+//        }
+//    }
+//});
+//
+//$( document ).ajaxError(function( event, jqxhr, settings ) {
+//    form = settings["form"];
+//    form_id = settings["form"];
+//    if (form_id) {
+//        var key = form_id.replace("form_", "");
+//        if (key) {
+//            var outer_div = "#detail_" + key;
+//            $(outer_div).removeClass("loading");
+//        }
+//    }
+//    }
+//});
