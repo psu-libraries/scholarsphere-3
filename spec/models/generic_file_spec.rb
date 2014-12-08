d9c5a24e8a6f4c7a329bb34695a3699d6dfad6d3
Adam Wead <amsterdamos@gmail.com>
2014-12-08 18:28:23 -0500

    Refactoring/removing model tests
    Many of our model tests duplicate those in Sufia. These duplications
    have been removed and other tests refactored to fit with Rspec3
    conventions.

--- spec/models/generic_file_spec.rb
+++ spec/models/generic_file_spec.rb
@@ -1,67 +1,50 @@
 require 'spec_helper'
 
-describe GenericFile, :type => :model do
+describe GenericFile, type: :model do
 
   let(:user) { FactoryGirl.create :random_user }
-  before(:each) do
-    @file = GenericFile.new
-    @file.apply_depositor_metadata(user.user_key)
+  let(:file) do
+    file = GenericFile.new(id: 'somepid')
+    file.apply_depositor_metadata(user.user_key)
+    return file
   end
 
   it 'should export as endnote' do
-    @file.stub(pid: 'stubbed_pid')
-    expect(@file.export_as_endnote).to eq("%0 GenericFile\n%R http://scholarsphere.psu.edu/files/stubbed_pid\n%~ ScholarSphere\n%W Penn State University")
+    expect(file.export_as_endnote).to eq("%0 GenericFile\n%R http://scholarsphere.psu.edu/files/somepid\n%~ ScholarSphere\n%W Penn State University")
   end
 
-  describe "create_thumbnail" do
+  describe "#create_thumbnail" do
     describe "with an image that doesn't get resized" do
-      before do
-        @f = GenericFile.new
-        @f.stub(mime_type:'image/png', width:['50'], height:['50'])  #Would get set by the characterization job
-        @f.add_file_datastream(File.open("#{Rails.root}/spec/fixtures/world.png", 'rb'), dsid:'content')
-        @f.apply_depositor_metadata('mjg36')
-        @f.save
-      end
-      after do
-        @f.delete
+      subject do
+        file.stub(mime_type:'image/png', width:['50'], height:['50'])  # Would get set by the characterization job
+        file.add_file_datastream(File.open("#{Rails.root}/spec/fixtures/world.png", 'rb'), dsid:'content')
+        file.save
+        return file
       end
       it "should keep the thumbnail at its original size" do
-        expect(@f.content.changed?).to be_falsey
+        expect(subject.content.changed?).to be_falsey
       end
     end
   end
 
-  describe "save" do
-    after(:each) do
-      @file.delete
-    end
+  describe "#save" do
     it "should schedule a characterization job" do
-      @file.add_file_datastream(File.new(Rails.root + 'spec/fixtures/world.png'), dsid:'content')
-      @file.save
+      file.add_file_datastream(File.new(Rails.root + 'spec/fixtures/world.png'), dsid:'content')
+      file.save
     end
   end
 
-  describe "characterize" do
+  describe "#characterize" do
     describe "after job runs" do
-      # File.new(Rails.root + 'spec/fixtures/scholarsphere/scholarsphere_test4.pdf')
-      before(:all) do
-        @myfile = GenericFile.new
-        @myfile.add_file(File.open(fixture_path + '/scholarsphere/scholarsphere_test4.pdf', 'rb').read, 'content', 'sufia_test4.pdf')
-        @myfile.label = 'label123'
-        @myfile.apply_depositor_metadata('mjg36')
-        @myfile.characterize
-      end
-      after(:all) do
-        @myfile.destroy
+      subject do
+        file.add_file(File.open(fixture_path + '/scholarsphere/scholarsphere_test4.pdf', 'rb').read, 'content', 'sufia_test4.pdf')
+        file.characterize
+        return file
       end
       it "should NOT append metadata from the characterization" do
-        expect(@myfile.title).not_to include("Microsoft Word - sample.pdf.docx")
-        expect(@myfile.filename[0]).to eq(@myfile.label)
-      end
-      it "should NOT append each term only once" do
-        @myfile.append_metadata
-        expect(@myfile.format_label).to eq(["Portable Document Format"])
-        expect(@myfile.title).not_to include("Microsoft Word - sample.pdf.docx")
+        expect(subject.title).not_to include("Microsoft Word - sample.pdf.docx")
+        expect(subject.filename[0]).to eq(subject.label)
+        expect(subject.format_label).to eq(["Portable Document Format"])
       end
     end
   end
