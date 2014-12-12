9d98a9c3f1343e2a84374c852b569cdc0f89891f
Adam Wead <amsterdamos@gmail.com>
2014-12-11 21:33:10 -0500

    Disabling load and authorized resource
    This is doing weird things and should be looked at later, but for now,
    it’s disabled. Chaneges will be addressed in #9678

--- app/controllers/generic_files_controller.rb
+++ app/controllers/generic_files_controller.rb
@@ -5,14 +5,15 @@ class GenericFilesController < ApplicationController
 
   around_action :notify_users_of_permission_changes, only: [:destroy,:create,:update]
   skip_before_action :has_access?, only: [:stats]
-  skip_load_resource(only: [:show])
-  before_filter :load_resource_from_solr, only: [:show]
-  authorize_resource only: [:show]
-
-  def load_resource_from_solr
-    @generic_file = GenericFile.load_instance_from_solr(params[:id])
-    @generic_file
-  end
+  
+  # TODO: load and authorize resource are causing problems (#9678)
+  #skip_load_resource(only: [:show])
+  #before_filter :load_resource_from_solr, only: [:show]
+  #authorize_resource only: [:show]
+  #def load_resource_from_solr
+  #  @generic_file = ::GenericFile.load_instance_from_solr(params[:id])
+  #  @generic_file
+  #end
 
   def notify_users_of_permission_changes
     previous_permissions = @generic_file.permissions unless @generic_file.nil?
