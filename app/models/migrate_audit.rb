5c72c7dab9269a780570103602eef7aee8caf0cd
Hector Correa <hector@hectorcorrea.com>
2015-02-18 20:18:01 -0500

    Added Fedora 4 audit method

--- app/models/migrate_audit.rb
+++ app/models/migrate_audit.rb
@@ -1,5 +1,7 @@
 class MigrateAudit < ActiveRecord::Base
 
+  # This method populates the migrateaudit table with data 
+  # from the Fedora 3 repo.
   def self.f3_audit(fedora_url, fedora_user, fedora_password, namespace)
     # destroy all existing audit records first
     all.each { |obj| obj.destroy! }
@@ -12,4 +14,15 @@ class MigrateAudit < ActiveRecord::Base
     end
   end
 
+  # This method makes sure each record in the migrateaudit table is
+  # in the Fedora 4 repo and it has the same model as its Fedora 3
+  # counterpart.
+  def self.f4_audit(fedora_url, fedora_user, fedora_password)
+    f3_data = all
+    audit = MigrateAuditFedora4.new(fedora_url, fedora_user, fedora_password)
+    results = audit.audit(f3_data)
+    results.each do |result|
+      puts result
+    end
+  end
 end
