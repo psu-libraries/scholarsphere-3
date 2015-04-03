860261ab5622d40948405cd8d28d0d128f607d7b
Carolyn Cole <cam156@psu.edu>
2015-04-03 14:23:34 -0400

    Adding my_collections_controller to override sufia and show only collections from the current user fixes #93

---
+++ app/controllers/my/collections_controller.rb
@@ -0,0 +1,21 @@
+# TODO:  This override should be removed once we upgrade sufia to 6.1
+module My
+  class CollectionsController < MyController
+
+    self.search_params_logic += [
+      :show_only_files_deposited_by_current_user,
+      :show_only_collections
+    ]
+
+    def index
+      super
+      @selected_tab = :collections
+    end
+
+    protected
+
+    def search_action_url *args
+      sufia.dashboard_collections_url *args
+    end
+  end
+end
