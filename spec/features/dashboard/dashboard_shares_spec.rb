28287c1c8637178e99c6a7a8fdfca4aa5fbee1f6
Carolyn Cole <cam156@psu.edu>
2015-04-07 12:00:20 -0400

    Setting edit access for shares so we only see files the user has edit access to.  fixes #94

--- spec/features/dashboard/dashboard_shares_spec.rb
+++ spec/features/dashboard/dashboard_shares_spec.rb
@@ -28,10 +28,31 @@ describe 'Dashboard Shares', :type => :feature do
         c.save!
       end
     }
+    let!(:gf) do
+      GenericFile.new.tap do |gf|
+        gf.title = ["jill's files"]
+        gf.filename = ['test.pdf']
+        gf.read_groups = ['public']
+        gf.apply_depositor_metadata("jilluser")
+        gf.save!
+      end
+    end
 
-    scenario 'does not display collections' do
+    let!(:gf2) do
+      GenericFile.new.tap do |gf|
+        gf.title = ["jill's shared file"]
+        gf.filename = ['test.pdf']
+        gf.permissions << Hydra::AccessControls::Permission.new(type: 'person', name: current_user.user_key, access: 'edit')
+        gf.apply_depositor_metadata("jilluser")
+        gf.save!
+      end
+    end
+
+    scenario 'does not display collections and others files' do
       go_to_dashboard_shares
       expect(page).to_not have_content(collection_title)
+      expect(page).to_not have_content(gf.title[0])
+      expect(page).to have_content(gf2.title[0])
     end
   end
 
