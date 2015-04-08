f820f6161c7ea3ab07e444a6f6821fdb558e6da2
Carolyn Cole <cam156@psu.edu>
2015-04-08 13:26:02 -0400

    adding namae parser for names to reverse the order of the creator.  fixes #119

---
+++ spec/controllers/batch_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe BatchController, type: :controller do
+  routes { Sufia::Engine.routes }
+  let (:subject) {BatchController.new}
+  let (:current_user) { User.new(display_name: "ALBERT EDWARD MATYASOVSKY JR") }
+  let (:batch) {Batch.new}
+
+  describe "#edit_form" do
+    before do
+      allow(subject).to receive(:current_user).and_return(current_user)
+      subject.instance_variable_set("@batch", batch)
+    end
+    it "creates an edit form with the creator in last name first ordering" do
+      expect(subject.edit_form[:creator]).to eq(["Matyasovsky, Jr, Albert Edward"])
+    end
+  end
+  
+
+end
