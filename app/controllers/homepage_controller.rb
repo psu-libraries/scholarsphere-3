d84f05eda67eae3ab26f544f94bde6295dff14b4
Carolyn Cole <cam156@psu.edu>
2015-07-15 10:01:14 -0400

    Adding a tiny mce announcement section for the home page

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,9 @@
+class HomepageController < ApplicationController
+  include Sufia::HomepageController
+
+  def index
+    super
+    @announcement_text = ContentBlock.find_or_create_by(name: 'annoucement_text')
+  end
+
+end
