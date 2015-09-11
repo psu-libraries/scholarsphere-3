06553cda45e13248670272c40a941e5b07ad6cc8
Carolyn Cole <cam156@psu.edu>
2015-09-11 14:25:50 -0400

    Only load the show page once and just check the link values

--- spec/features/generic_file/view_and_download_spec.rb
+++ spec/features/generic_file/view_and_download_spec.rb
@@ -52,36 +52,37 @@ describe 'Generic File viewing and downloading:', :type => :feature do
         expect(page).to have_link 'http://example.org/TheDescriptionLink/'
         expect(page).to have_link file_1.related_url.first
 
+        # get an array of all the links we are testing
+        test_links = {}
+
         # 'I can see the link for creator and it filters correctly' do
-        test_link file_1.creator.first
-        visit "/files/#{file_1.id}"
+        test_links = store_link file_1.creator.first, test_links
 
         # 'I can see the link for contributor and it filters correctly' do
-        test_link file_1.contributor.first
-        visit "/files/#{file_1.id}"
+        test_links = store_link file_1.contributor.first, test_links
 
         # 'I can see the link for publisher and it filters correctly' do
-        test_link file_1.publisher.first
-        visit "/files/#{file_1.id}"
+        test_links = store_link file_1.publisher.first, test_links
 
         # 'I can see the link for subject and it filters correctly' do
-        test_link file_1.subject.first
-        visit "/files/#{file_1.id}"
+        test_links = store_link file_1.subject.first, test_links
 
         # 'I can see the link for language and it filters correctly' do
-        test_link file_1.language.first
-        visit "/files/#{file_1.id}"
+        test_links = store_link file_1.language.first, test_links
 
         # 'I can see the link for based_near and it filters correctly' do
-        test_link file_1.based_near.first
-        visit "/files/#{file_1.id}"
+        test_links = store_link file_1.based_near.first, test_links
 
         # 'I can see the link for a tag and it filters correctly' do
-        test_link file_1.tag.first
-        visit "/files/#{file_1.id}"
+        test_links = store_link file_1.tag.first, test_links
 
         # 'I can see the link for rights and it filters correctly' do
-        test_link Sufia.config.cc_licenses_reverse[file_1.rights.first]
+        test_links= store_link Sufia.config.cc_licenses_reverse[file_1.rights.first], test_links
+
+        #loop through all links
+        test_links.each do |name, link|
+          test_link name, link
+        end
       end
 
 
@@ -142,9 +143,14 @@ describe 'Generic File viewing and downloading:', :type => :feature do
 
   end
 
-  def test_link link_name
+  def store_link  link_name, test_links
     expect(page).to have_link link_name
-    click_on link_name
+    test_links[file_1.tag.first] = find_link(file_1.tag.first)[:href]
+    test_links
+  end
+
+  def test_link link_name, link_value
+    visit link_value
     expect(page).to have_content file_1.title.first
     expect(page).not_to have_content file_2.title.first
   end
