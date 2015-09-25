102c14a7ffa56f2f08b16264215627083c8d4279
Carolyn Cole <cam156@psu.edu>
2015-09-25 07:34:47 -0400

    Changing email strings to I18N

--- app/models/stats_mailer.rb
+++ app/models/stats_mailer.rb
@@ -3,8 +3,8 @@ class StatsMailer < ActionMailer::Base
   def stats_mail(start_datetime, end_datetime)
     @presenter = StatsPresenter.new(start_datetime, end_datetime)
 
-    attachments["scholarsphere-stats_#{@presenter.date_str}.csv"] = GenericFileListToCSVService.new(::GenericFile.find_by_date_created(start_datetime, end_datetime)).csv
+    attachments[::I18n.t("statistic.report.csv.file_name", date_str: @presenter.date_str)] = GenericFileListToCSVService.new(::GenericFile.find_by_date_created(start_datetime, end_datetime)).csv
 
-    mail( to: ScholarSphere::Application.config.stats_email, from: ScholarSphere::Application.config.stats_from_email, subject: "ScholarSphere - Statistic Report")
+    mail( to: ScholarSphere::Application.config.stats_email, from: ScholarSphere::Application.config.stats_from_email, subject: ::I18n.t("statistic.report.subject"))
   end
 end
