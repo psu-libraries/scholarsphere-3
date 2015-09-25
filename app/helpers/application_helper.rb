a3cfc1f8daf72c91fa6827c15cde74d811df8ecc
Carolyn Cole <cam156@psu.edu>
2015-09-25 13:12:17 -0400

    Adding help to on behalf of. fixes #191 Also fixed some rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,22 @@
 module ApplicationHelper
-  def has_collection_search_parameters?
+  def collection_search_parameters?
     !params[:cq].blank?
   end
 
   def collection_helper_method(value)
     c = Collection.load_instance_from_solr(value)
     c.title
+  end
+
+  def link_to_help(subject)
+    link_to '#', id: "#{subject}_help", rel: 'popover'.freeze,
+                 'data-content' => t("hydra.metadata_help.#{subject}_html"), 'data-original-title' => subject.titleize,
+                 'aria-label' => "Help for #{subject.titleize}" do
+      help_icon
+    end
+  end
+
+  def help_icon
+    content_tag 'i', nil, "aria-hidden" => true, class: "help-icon"
   end
 end
--- app/helpers/permission_helper.rb
+++ app/helpers/permission_helper.rb
@@ -2,19 +2,19 @@ module PermissionHelper
   def permission_level_tag(document)
     if document.registered?
       value = t('sufia.institution_name')
-      className = "label-info"
+      class_name = "label-info"
     elsif document.public?
       value = "Open Access"
-      className = "label-success"
+      class_name = "label-success"
     else
       value = "Private"
-      className = "label-danger"
+      class_name = "label-danger"
     end
     if can? :edit, document
       path = sufia.edit_generic_file_path(document, tab: "permissions")
     else
       path = sufia.generic_file_path(document)
     end
-    link_to content_tag(:span, value, class: "label #{className}", title: value), path, id: "permission_#{document.id}"
+    link_to content_tag(:span, value, class: "label #{class_name}", title: value), path, id: "permission_#{document.id}"
   end
 end
