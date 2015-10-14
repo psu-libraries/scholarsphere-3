ea66809148002edabd7394a0f7c2a23e24eb9fa9
Justin Patterson <jrp22@psu.edu>
2015-10-14 11:39:29 -0400

    Consolidated Passenger task does not require its own file anymore

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,11 +65,6 @@ set :pty, true
 # Default value for keep_releases is 5, setting to 7
 set :keep_releases, 7
 
-# capistrano-passenger gem values
-# Until Passenger allows sudoless restarts this needs to be in place as, we restrict sudo commands and the sudo command needed would have to change with every ruby version change because of the rbenv prefix.
-set :passenger_restart_command, "sudo service httpd restart"
-set :passenger_restart_options, -> {}
-
 # Apache namespace to control apache
 namespace :apache do
   [:stop, :start, :restart, :reload].each do |action|
@@ -141,8 +136,39 @@ namespace :deploy do
       end
     end
   end
+
+# Passenger Capistrano Task
+# The passenger install task allows Chef to install Passenger now via Yum, but it allows Capistrano to maintain the file
+# as Ruby is updated on the system.  The PassengerDefaultRuby variable is set to system ruby by default from the Yum
+# install.  This will not work in our environment.
+# Passenger Install Task below defines the current ruby version
+# Adds it to temp file
+# then copies passenger configs to temp file.
+# Replaces all instances of PassengerRuby with proper version in temp file.
+# Replace passenger conf file with temp file.
+
+namespace :passenger do
+  desc "Passenger Version Config Update"
+  task :config_update do
+   on roles(:web)  do
+    execute "mkdir --parents /opt/heracles/deploy/passenger"
+    execute 'cd ~deploy/scholarsphere/current && echo -n "PassengerRuby " > ~deploy/passenger/passenger-ruby-version.cap   && rbenv which ruby >> ~deploy/passenger/passenger-ruby-version.cap'
+    execute 'v_passenger_ruby=$(cat ~deploy/passenger/passenger-ruby-version.cap) &&    cp --force /etc/httpd/conf.d/phusion-passenger-default-ruby.conf ~deploy/passenger/passenger-ruby-version.tmp &&    sed -i -e "s|.*PassengerRuby.*|${v_passenger_ruby}|" ~deploy/passenger/passenger-ruby-version.tmp &&     sudo /bin/mv ~deploy/passenger/passenger-ruby-version.tmp /etc/httpd/conf.d/phusion-passenger-default-ruby.conf &&  sudo /sbin/service httpd restart'    
+    end
+  end
+
+  desc "warm up passenger"
+  task :warmup do
+   on roles(:web) do
+   puts "do something"
+   #execute "curl -s -k --head https://$(hostname -f)"
+   # execute "curl -s -k -o /dev/null --head https://$(hostname -f)"
+   end
+  end
+end
+
   after :resquepoolrestart, :sitemapxml
-  after :published, "passenger:install"
+  after :published, "passenger:config_update"
   # after :publishing, :restart
   after :restart, "passenger:warmup"
 end
