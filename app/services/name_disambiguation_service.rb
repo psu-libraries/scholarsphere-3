704ef8046bade827c26e36ebab0315a8e4525b92
Adam Wead <amsterdamos@gmail.com>
2015-10-19 14:32:16 -0400

    Refactoring NameDisambiguationService

--- app/services/name_disambiguation_service.rb
+++ app/services/name_disambiguation_service.rb
@@ -1,111 +1,118 @@
 class NameDisambiguationService
-  attr_reader :name, :email_for_name
+  attr_reader :name, :email_for_name_cache, :results
 
   def initialize(name)
     @name = name
-    @email_for_name = {}
+    @email_for_name_cache = {}
+    @results = []
   end
 
   def disambiguate
-    # check if it is an ID
-    login_results = login_name(name)
-    return [login_results] unless login_results.blank?
-
-    email_results = email_in_name(name)
-    return [email_results] unless email_results.blank?
-
-    results = []
-
-    # check for multiples
-    names  = name.split(/and|;/)
-    names.each do |name|
-      name.gsub!(/\([^)]*\)/,"")
-      name.strip!
-      # parse each name
-      # call to ldap to get the email and id
-      email_results = email_for_name(name)
-      results << email_results unless email_results.blank?
-    end
+    query_result = login_name(name) || email_in_name(name)
+    results << query_result unless query_result.blank?
+    multiple_names(name) if results.blank?
     results
   end
 
   private
 
-    private
-      def login_name(name)
-        attrs = User.directory_attributes(name, [:uid, :givenname, :sn, :mail, :eduPersonPrimaryAffiliation])
-        return nil if attrs.count < 1
-        attrs = attrs[0]
-        {id: attrs[:uid][0], given_name: attrs[:givenname][0], surname: attrs[:sn][0],
-           email: attrs[:mail][0] , affiliation: attrs[:eduPersonPrimaryAffiliation]}
+    def login_name(id)
+      attrs = User.directory_attributes(id, [:uid, :givenname, :sn, :mail, :eduPersonPrimaryAffiliation])
+      return nil if attrs.count < 1
+      results_hash(attrs.first)
+    end
+
+    # "thing" is an awful name - it's a placeholder to make this a pure function
+    def email_in_name(thing)
+      return unless thing.include?("@")
+      parts = thing.split(" ")
+      email = parts.reject{|part| !part.include?("@")}[0]
+      id = email.split('@')[0]
+      login_name(id) || results_hash(mail: [email])
+    end
+
+    def multiple_names(multi_name)
+      multi_name.split(/and|;/).each do |n|
+        n.gsub!(/\([^)]*\)/,"")
+        n.strip!
+        query_result = email_for_name(n)
+        results << query_result unless query_result.blank?
+      end
+    end
+
+    def results_hash(opts)
+      {
+        id:          opts.fetch(:uid, [""]).first,
+        given_name:  opts.fetch(:givenname, [""]).first,
+        surname:     opts.fetch(:sn, [""]).first,
+        email:       opts.fetch(:mail).first,
+        affiliation: opts.fetch(:eduPersonPrimaryAffiliation, []),
+      }
+    end
+
+    def email_for_name(emailname)
+      return "" if emailname.blank?
+      return @email_for_name_cache[emailname] unless @email_for_name_cache[emailname].blank?
+
+      # normal name
+      emailname.gsub!(/[^\w\s,']/, ' ')
+      parsed = Namae::Name.parse(emailname)
+      result = try_name(parsed.given, parsed.family)
+
+      result ||= title_before_name(parsed) || two_words_in_last_name(emailname) || title_after_name(emailname)
+
+      if result.nil?
+        puts "got zero for #{emailname}"
       end
 
-      def email_in_name(name)
-        return unless name.include?("@")
-        parts =  name.split(" ")
-        email = parts.reject{|part| !part.include?("@")}[0]
-        id = email.split('@')[0]
-        result = login_name(id)
-        result ||= {id: "", given_name: "", surname: "",
-            email: email , affiliation: []}
+      @email_for_name_cache[emailname] = result
+      return @email_for_name_cache[emailname]
+    end
+
+    def try_name(given, family)
+      return nil if family.blank?
+      possible_users  = User.query_ldap_by_name(given, family)
+      return nil if possible_users.count == 0
+      if possible_users.count > 1
+        puts "Returning #{possible_users.first} but got more than name for given name #{given} and family name #{name}"
       end
+      possible_users.first
+    end
 
-      def email_for_name( name)
-        return "" if name.blank?
-        return @email_for_name[name] unless @email_for_name[name].blank?
+    # namething is an awful variable name, but it distinguished this var from the class "name" var
+    def name_parts(namething, count)
+      parts = namething.split(" ")
+      first_name_count = parts.count - count
+      return nil if count < 1
+      {given: parts.first(first_name_count).join(" "), family: parts.last(count).join(" ")}
+    end
 
-        # normal name
-        name.gsub!(/[^\w\s,']/, ' ')
-        parsed = Namae::Name.parse(name)
-        result = try_name(parsed.given, parsed.family)
-        if result == ""
-          puts "got more than one for #{name}"
-        end
+   def title_before_name(parsed)
+     return unless parsed
+     result = nil
+     if parsed.given && parsed.given.count(" ") >= 1
+       parts = name_parts(parsed.given,1)
+       result = try_name(parts[:family], parsed.family)
+     end
+     result
+   end
 
-        # title before the name
-        if result.nil? &&  parsed.given &&parsed.given.count(" ") >= 1
-          parts = name_parts(parsed.given,1)
-          result = try_name(parts[:family], parsed.family)
-        end
+   def two_words_in_last_name(emailname)
+     result = nil
+     if emailname.count(" ") > 2
+       parts = name_parts(emailname,2)
+       result = try_name(parts[:given], parts[:family])
+     end
+     result
+   end
 
-        # two words in the last name
-        if result.nil? && name.count(" ") > 2
-          parts = name_parts(name,2)
-          result = try_name(parts[:given], parts[:family])
-        end
-
-        # titles after the name that namae had trouble parsing
-        if result.nil? && name.count(',') > 0
-          new_name = name.split(',')[0]
-          result = email_for_name( new_name) if new_name.count(' ') > 0
-        end
-
-        if result.nil?
-          puts "got zero for #{name}"
-        end
-
-        @email_for_name[name] = result
-        return @email_for_name[name]
+   # titles after the name that namae had trouble parsing
+   def title_after_name(emailname)
+     result = nil
+     if emailname.count(',') > 0
+        new_name = emailname.split(',')[0]
+        result = email_for_name(new_name) if new_name.count(' ') > 0
       end
-
-      def try_name(given, family)
-        return nil if family.blank?
-        possible_users  = User.query_ldap_by_name(given, family)
-        case possible_users.count
-          when 1
-            possible_users[0]
-          when 0
-            nil
-          else
-            ""
-        end
-      end
-
-      def name_parts(name, count)
-        parts = name.split(" ")
-        first_name_count = parts.count - count
-        return nil if count < 1
-        {given: parts.first(first_name_count).join(" "), family: parts.last(count).join(" ")}
-      end
-
+      result
+   end
 end
