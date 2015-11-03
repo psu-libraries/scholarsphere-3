33ac0fa94bb0febd20b779e2cdb7216debfac782
Adam Wead <amsterdamos@gmail.com>
2015-11-03 17:47:13 -0500

    ScholarsphereBatchUpdateJob pushes share notify jobs, fixes #202

--- app/controllers/batch_controller.rb
+++ app/controllers/batch_controller.rb
@@ -7,4 +7,16 @@ class BatchController < ApplicationController
     generic_file = ::GenericFile.new(creator: [parsed.sort_order], title: @batch.generic_files.map(&:label))
     edit_form_class.new(generic_file)
   end
+
+  protected
+
+    def batch_update_job
+      @batch_update_job ||= ScholarsphereBatchUpdateJob.new(
+        current_user.user_key,
+        params[:id],
+        params[:title],
+        edit_form_class.model_attributes(params[:generic_file]),
+        params[:visibility])
+    end
+
 end
---
+++ app/jobs/scholarsphere_batch_update_job.rb
@@ -0,0 +1,8 @@
+class ScholarsphereBatchUpdateJob < BatchUpdateJob
+
+  def queue_additional_jobs(gf)
+    super
+    Sufia.queue.push(ShareNotifyJob.new(gf.id))
+  end
+
+end
