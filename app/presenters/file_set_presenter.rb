7cfaba2f4c30fb29c10d9abcda77a3d43d2fd0ef
Adam Wead <amsterdamos@gmail.com>
2016-04-14 14:37:48 -0400

    What should be do about related files?

--- app/presenters/file_set_presenter.rb
+++ app/presenters/file_set_presenter.rb
@@ -3,6 +3,6 @@ class FileSetPresenter < Sufia::FileSetPresenter
   # See https://github.com/projecthydra/sufia/issues/1478
   # TODO: What do we want to do about related files?
   def related_files
-    super
+    []
   end
 end
