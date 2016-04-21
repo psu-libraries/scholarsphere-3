3d82efb751ee001a31fb43d1fce178423525b5c4
Adam Wead <amsterdamos@gmail.com>
2016-04-21 12:04:02 -0400

    Deleting all homepage overrides

--- app/controllers/homepage_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-class HomepageController < ApplicationController
-  include Sufia::HomepageControllerBehavior
-
-  def index
-    super
-    @announcement_text = ContentBlock.find_or_create_by(name: 'annoucement_text')
-  end
-end
