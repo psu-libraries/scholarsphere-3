cd7008f652bfd8592778d2014ecf68bf05bad90b
Adam Wead <amsterdamos@gmail.com>
2016-05-03 15:09:28 -0400

    Rubocop fixes

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 class DownloadsController < ApplicationController
-
   prepend_before_action only: [:show] do
     handle_legacy_url_prefix { |new_id| redirect_to sufia.download_path(new_id), status: :moved_permanently }
   end
