2bf88aed610da45e6e03bd02c35c49a8816e2a07
Adam Wead <amsterdamos@gmail.com>
2016-05-04 10:27:37 -0400

    Updating Redis config

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,12 +1,27 @@
 # frozen_string_literal: true
-# Code borrowed from Obie's Redis patterns talk at RailsConf'12
-Nest.class_eval do
-  def initialize(key, redis = $redis)
-    super(key.to_param)
-    @redis = redis
-  end
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # We're in smart spawning mode.
+    if forked
+      # Re-establish redis connection
+      require 'redis'
+      config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
 
-  def [](key)
-    self.class.new("#{self}:#{key.to_param}", @redis)
+      # The important two lines
+      Redis.current.disconnect!
+      Redis.current = begin
+                        Redis.new(config.merge(thread_safe: true))
+                      rescue
+                        nil
+                      end
+    end
   end
+else
+  config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+  require 'redis'
+  Redis.current = begin
+             Redis.new(config.merge(thread_safe: true))
+           rescue
+             nil
+           end
 end
