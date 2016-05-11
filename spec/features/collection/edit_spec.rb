f527a275044e31d5b5109eccc8ebb923a21d3ec2
Carolyn Cole <cam156@psu.edu>
2016-05-11 07:40:46 -0400

    Fixing up collection factory to apply depositor correctly and fix one test in collection_edit_spec.rb

--- spec/features/collection/edit_spec.rb
+++ spec/features/collection/edit_spec.rb
@@ -6,56 +6,56 @@ include Selectors::EditCollections
 
 describe Collection, type: :feature do
   let(:current_user) { create(:user) }
-  let(:file1)        { create(:file, depositor: current_user.login, title: ['world.png']) }
-  let(:file2)        { create(:file, depositor: current_user.login, title: ['little_file.txt']) }
+  let(:work1)        { create(:work, depositor: current_user.login, title: ['world.png']) }
+  let(:work2)        { create(:work, depositor: current_user.login, title: ['little_file.txt']) }
 
   before { sign_in_with_js(current_user) }
 
   context "when the collection has files" do
-    let!(:collection) { create(:collection, depositor: current_user.login, members: [file1, file2]) }
-    let!(:file3)      { create(:file, title: ['scholarsphere_test5.txt'], depositor: current_user.login) }
+    let!(:collection) { create(:collection, depositor: current_user.login, members: [work1, work2]) }
+    let!(:work3)      { create(:work, title: ['scholarsphere_test5.txt'], depositor: current_user.login) }
 
     describe 'adding an additional file' do
       specify do
-        visit '/dashboard/files'
-        db_file_checkbox(file3).click
+        visit '/dashboard/works'
+        db_file_checkbox(work3).click
         click_button 'Add to Collection'
         db_collection_radio_button(collection).click
         within("#collection-list-container .modal-footer") do
           click_button 'Add to Collection'
         end
         expect(page).to have_content 'Collection was successfully updated.'
-        expect(page).to have_content file3.title.first
+        expect(page).to have_content work3.title.first
       end
     end
-    describe 'removing a file' do
+    describe 'removing a work' do
       specify do
         visit '/dashboard/collections'
         db_item_actions_toggle(collection).click
         click_link 'Edit Collection'
-        expect(page).to have_content file1.title.first
-        expect(page).to have_content file2.title.first
-        db_item_actions_toggle(file1).click
+        expect(page).to have_content work1.title.first
+        expect(page).to have_content work2.title.first
+        db_item_actions_toggle(work1).click
         click_button 'Remove from Collection'
         expect(page).to have_content collection.title
         expect(page).to have_content collection.description
-        expect(page).not_to have_content file1.title.first
-        expect(page).to have_content file2.title.first
+        expect(page).not_to have_content work1.title.first
+        expect(page).to have_content work2.title.first
       end
     end
-    describe 'removing all files' do
+    describe 'removing all works' do
       specify do
         visit '/dashboard/collections'
         db_item_actions_toggle(collection).click
         click_link 'Edit Collection'
-        expect(page).to have_content file1.title.first
-        expect(page).to have_content file2.title.first
+        expect(page).to have_content work1.title.first
+        expect(page).to have_content work2.title.first
         check 'check_all'
         click_button 'Remove From Collection'
         expect(page).to have_content collection.title
         expect(page).to have_content collection.description
-        expect(page).not_to have_content file1.title.first
-        expect(page).not_to have_content file2.title.first
+        expect(page).not_to have_content work1.title.first
+        expect(page).not_to have_content work2.title.first
       end
     end
   end
