d14465e744fbac542b0ea18e9b773f3ba15aad08
Carolyn Cole <cam156@psu.edu>
2016-05-12 08:09:26 -0400

    Fixing up feature tests now that collection has multiple titles and decriptions

--- spec/features/collection/edit_spec.rb
+++ spec/features/collection/edit_spec.rb
@@ -12,7 +12,7 @@ describe Collection, type: :feature do
   before { sign_in_with_js(current_user) }
 
   context "when the collection has files" do
-    let!(:collection) { create(:collection, depositor: current_user.login, members: [work1, work2]) }
+    let!(:collection) { create(:collection, depositor: current_user.login, members: [work1, work2], description: ["my description"]) }
     let!(:work3)      { create(:work, title: ['scholarsphere_test5.txt'], depositor: current_user.login) }
 
     describe 'adding an additional file' do
@@ -37,8 +37,8 @@ describe Collection, type: :feature do
         expect(page).to have_content work2.title.first
         db_item_actions_toggle(work1).click
         click_button 'Remove from Collection'
-        expect(page).to have_content collection.title
-        expect(page).to have_content collection.description
+        expect(page).to have_content collection.title.first
+        expect(page).to have_content collection.description.first
         expect(page).not_to have_content work1.title.first
         expect(page).to have_content work2.title.first
       end
@@ -52,8 +52,8 @@ describe Collection, type: :feature do
         expect(page).to have_content work2.title.first
         check 'check_all'
         click_button 'Remove From Collection'
-        expect(page).to have_content collection.title
-        expect(page).to have_content collection.description
+        expect(page).to have_content collection.title.first
+        expect(page).to have_content collection.description.first
         expect(page).not_to have_content work1.title.first
         expect(page).not_to have_content work2.title.first
       end
@@ -61,26 +61,28 @@ describe Collection, type: :feature do
   end
 
   describe "editing a collection's metadata" do
-    let!(:collection)           { create(:collection, depositor: current_user.login) }
-    let!(:original_title)       { collection.title }
-    let!(:original_description) { collection.description }
-
-    let(:updated_title)         { 'Updated Title' }
-    let(:updated_description)   { 'Updtaed description text.' }
-    let(:updated_creators)      { ['Dorje Trollo', 'Vajrayogini'] }
+    # let!(:collection)           { create(:collection, depositor: current_user.login, description: ["original description"]) }
+    # let!(:original_title)       { collection.title }
+    # let!(:original_description) { collection.description }
+    #
+    # let(:updated_title)         { 'Updated Title' }
+    # let(:updated_description)   { 'Updtaed description text.' }
+    # let(:updated_creators)      { ['Dorje Trollo', 'Vajrayogini'] }
 
     specify do
+      visit '/dashboard/highlights'
+      save_screenshot('/Users/cam156/scholarsphere/file.png', :full => true)
       visit '/dashboard/collections'
       db_item_actions_toggle(collection).click
       click_link 'Edit Collection'
-      expect(page).to have_field 'collection_title', with: original_title
-      expect(page).to have_field 'collection_description', with: original_description
+      expect(page).to have_field 'collection_title', with: original_title.first
+      expect(page).to have_field 'collection_description', with: original_description.first
       fill_in 'Title', with: updated_title
       fill_in 'Description', with: updated_description
       fill_in 'Creator', with: updated_creators.first
       ec_update_submit.click
-      expect(page).not_to have_content original_title
-      expect(page).not_to have_content original_description
+      expect(page).not_to have_content original_title.first
+      expect(page).not_to have_content original_description.first
       expect(page).to have_content updated_title
       expect(page).to have_content updated_description
       expect(page).to have_content updated_creators.first
