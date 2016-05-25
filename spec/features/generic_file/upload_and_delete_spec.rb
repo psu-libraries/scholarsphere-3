9bd6423eafe1dd43ce6ec1d825aa8ecd4979e2c5
Hector Correa <hector@hectorcorrea.com>
2016-05-25 15:04:14 -0400

    Got all the tests to pass (except Cloud providers)

--- spec/features/generic_file/upload_and_delete_spec.rb
+++ spec/features/generic_file/upload_and_delete_spec.rb
@@ -4,12 +4,14 @@ require 'feature_spec_helper'
 include Selectors::Dashboard
 
 describe 'Generic File uploading and deletion:', type: :feature do
+  let(:new_generic_work_path) { '/concern/generic_works/new' }
   context 'When logged in as a PSU user' do
-    let(:current_user)  { create(:user) }
-    let(:other_user)    { create(:user) }
-    let(:filename)      { 'little_file.txt' }
-    let(:batch)         { ['little_file.txt', 'little_file.txt'] }
-    let(:file)          { find_file_by_title "little_file.txt" }
+    let(:current_user)          { create(:user) }
+    let(:other_user)            { create(:user) }
+    let(:filename)              { 'little_file.txt' }
+    let(:batch)                 { ['little_file.txt', 'little_file.txt'] }
+    let(:file)                  { work }
+    let(:work)                  { find_work_by_title "little_file.txt_title" }
 
     before do
       sign_in_with_js(current_user)
@@ -17,7 +19,7 @@ describe 'Generic File uploading and deletion:', type: :feature do
 
     context 'the user agreement' do
       before do
-        Sufia::Engine.routes.url_helpers.new_generic_file_path
+        visit new_generic_work_path
       end
       it "does not show Sufia's user agreement" do
         expect(page).not_to have_content("Sufia's Deposit Agreement")
@@ -26,60 +28,52 @@ describe 'Generic File uploading and deletion:', type: :feature do
 
     context 'user needs help' do
       before do
-        visit Sufia::Engine.routes.url_helpers.new_generic_file_path
-        expect(page).to have_content "Agree to the deposit agreement and then select files.  Press the Start Upload Button once all files have been selected"
-        check 'terms_of_service'
+        visit new_generic_work_path
+        # TODO: It seems that we removed this screen reader only text from the upload form in Sufia 7.
+        #       Added ticket to address it: https://github.com/psu-stewardship/scholarsphere/issues/308
+        #
+        # expect(page).to have_content "Agree to the deposit agreement and then select files.  Press the Start Upload Button once all files have been selected"
+        #
         attach_file('files[]', test_file_path(filename), visible: false)
-        redirect_url = find("#redirect-loc", visible: false).text(:all)
-        click_button 'main_upload_start'
-        wait_for_page redirect_url
-        expect(page).to have_content 'Apply Metadata'
+        check 'agreement'
       end
 
       specify 'I can view help for rights, visibility, and share with' do
         # I can add additional rights
         expect(User).to receive(:query_ldap_by_name_or_id).and_return([{ id: other_user.user_key, text: "#{other_user.display_name} (#{other_user.user_key})" }])
-        find('.select2-container').click
-        sleep(1)
-        find('#select2-drop .select2-input').set other_user.user_key
-        find('#select2-drop .select2-result-selectable').click
+        click_link('Share')
+        expect(page).to have_css('a.select2-choice')
+        first('a.select2-choice').click
+        find(".select2-input").set(other_user.user_key)
+        expect(page).to have_css('div.select2-result-label')
+        first('div.select2-result-label').click
         find('#new_user_permission_skel').find(:xpath, 'option[2]').select_option
-        click_on "add_new_user_skel"
-        expect(page).to have_css("label.control-label", text: other_user.user_key)
-
-        # I am adding can click on more metadata here so we do not need to add a separate test for it
-        expect(page).not_to have_css("#generic_file_publisher")
-        click_on 'Show Additional Fields'
-        expect(page).to have_css("#generic_file_publisher")
-        click_on 'Hide Additional Fields'
-        expect(page).not_to have_css("#generic_file_publisher")
-
-        # If these tests start to randomly fail please consider re-adding
-        # the calls to sleep and save_and_open that I removed because they
-        # slowed down the execution of the tests. - Hector 7/7/2014
-
-        # Visibility is a tooltip. Click on it once to show it,
-        # click again to hide it.
-        within('#visibility_tooltip') do
-          find('.help-icon').trigger('click')
-          expect(page).to have_css('h3.popover-title', text: 'Visibility')
-          find('.help-icon').trigger('click')
-          expect(page).not_to have_css('h3.popover-title', text: 'Visibility')
+        click_on('add_new_user_skel')
+        within("#share") do
+          expect(page).to have_content(other_user.user_key)
         end
 
-        # Share With is a tooltip. Click on it once to show it,
-        # click again to hide it.
-        within('#share_with_tooltip') do
-          find('.help-icon').trigger('click')
-          expect(page).to have_css('h3.popover-title', text: 'Share With')
-          find('.help-icon').trigger('click')
-          expect(page).not_to have_css('h3.popover-title', text: 'Share With')
+        # I am adding can click on more metadata here so we do not need to add a separate test for it
+        click_link("Metadata")
+        expect(page).to have_no_css("#generic_work_contributor")
+        click_link("Additional fields")
+        expect(page).to have_css("#generic_work_contributor")
+        expect(page).to have_css(".collapse.in") # wait for JavaScript to collapse fields
+        click_link("Additional fields")
+        expect(page).to have_no_css("#generic_work_contributor")
+
+        within("#savewidget") do
+          expect(page).to have_content("Visibility")
+          expect(page).to have_content("Public")
+          expect(page).to have_content("Embargo")
+          expect(page).to have_content("Private")
+          expect(page).to have_content("Penn State")
         end
 
         # Rights (i.e. License Descriptions) is a modal form
         # with a close button.
         expect(page).not_to have_css('#rightsModal')
-        within('#generic_file_rights_help_modal') do
+        within('#generic_work_rights_help_modal') do
           find('.help-icon').click
         end
         expect(page).to have_css('#rightsModal')
@@ -88,17 +82,9 @@ describe 'Generic File uploading and deletion:', type: :feature do
         expect(modal[:style]).to match(/display: block/)
         expect(page).to have_content('Creative Commons licenses can take the following combinations')
         click_on('Close')
-
-        # The following tests might work in your local environment
-        # but randomly fail in Travis/Jenkins
-        #
-        # modal_closed = find('#rightsModal', visible: false)
-        # expect(modal_closed[:style]).to match(/display: none/)
-        #
-        # expect(page).not_to have_content('Creative Commons licenses can take the following combinations')
-        # page.should_not have_selector(:css, '#rightsModal')
       end
     end
+
     context 'cloud providers' do
       before do
         allow(BrowseEverything).to receive(:config) { { "dropbox" => { app_key: "fakekey189274942347", app_secret: "fakesecret489289472347298", max_upload_file_size: 20 * 1024 } } }
@@ -154,26 +140,28 @@ describe 'Generic File uploading and deletion:', type: :feature do
     context 'user does not need help' do
       context 'with a single file' do
         before do
-          upload_generic_file filename
+          create_work_and_upload_file(filename)
         end
         specify 'uploading, deleting and notifications' do
-          expect(page).to have_css '#documents'
-          expect(page).to have_content filename
-          click_link "dashboard_link"
+          click_link "My Dashboard"
           expect(page).to have_css "table#activity"
           within("table#activity") do
             expect(page).to have_content filename
           end
-          within("#notifications") do
-            expect(page).to have_content "Batch upload complete"
-            expect(page).to have_content "less than a minute ago"
-            expect(page).to have_content filename
-            expect(page).to have_content "has been saved."
-          end
-          go_to_dashboard_files
+          # TODO: Re-enable notifications after a file has been added.
+          #       See https://github.com/psu-stewardship/scholarsphere/issues/311
+          #
+          # within("#notifications") do
+          #   expect(page).to have_content "Batch upload complete"
+          #   expect(page).to have_content "less than a minute ago"
+          #   expect(page).to have_content filename
+          #   expect(page).to have_content "has been saved."
+          # end
+          #
+          go_to_dashboard_works
           expect(page).to have_content file.title.first
           db_item_actions_toggle(file).click
-          click_link 'Delete File'
+          click_link 'Delete Work'
           expect(page).not_to have_content file.title.first
         end
       end
@@ -186,7 +174,7 @@ describe 'Generic File uploading and deletion:', type: :feature do
     before { sign_in_with_js(current_user) }
 
     specify 'I cannot access the upload page' do
-      visit Sufia::Engine.routes.url_helpers.new_generic_file_path
+      visit new_generic_work_path
       expect(page).to have_content 'Unauthorized'
       expect(page).not_to have_content 'Upload'
     end
---
+++ spec/support/helpers/generic_works.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+module GenericWorksHelper
+  def wait_for_page(redirect_url)
+    Timeout.timeout(Capybara.default_max_wait_time * 5) do
+      loop until current_path == redirect_url
+    end
+  end
+
+  def create_work_and_upload_file(filename)
+    visit '/concern/generic_works/new'
+    check 'agreement'
+    click_on 'Files'
+    attach_file('files[]', test_file_path(filename), visible: false)
+    click_on 'Metadata'
+    fill_in 'generic_work_title', with: filename + '_title'
+    fill_in 'generic_work_tag', with: filename + '_tag'
+    fill_in 'generic_work_creator', with: filename + '_creator'
+    select 'Attribution-NonCommercial-NoDerivs 3.0 United States', from: 'generic_work_rights'
+    click_on 'Save'
+    expect(page).to have_css('h1', filename + '_title')
+  end
+
+  def find_work_by_title(title)
+    GenericWork.where(Solrizer.solr_name("title", :stored_searchable, type: :string) => title).first
+  end
+end
+
+RSpec.configure do |config|
+  config.include GenericWorksHelper
+end
