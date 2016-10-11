dfba2770742551896aaef23ec38f05818107327f
mtribone <mtribone@psu.edu>
2016-10-11 12:31:40 -0400

    Removes a lot of old images, creates a service for the collection thumbnail, adds new collection png. Adds indexer and refs image path, fixes rubocop errors.
    Collection indexer inherits from CC Indexer, namespace Sufia for better compatibility, updates collection.rb for new collection indexer.
    
    Tests for the presence of the correct collection PNG and not the default image.
    
    Trying the have_css matcher to test the presence of the correct png file.
    
    Fixes the test by using ensuring that the thumbnail path starts with assets/collection

---
+++ spec/indexers/collection_indexer_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe Sufia::CollectionIndexer do
+  describe "fontawesome default icon" do
+    let(:collection) { build(:collection, id: "1234") }
+    let(:solr_doc) { described_class.new(collection).generate_solr_document }
+
+    it "indexes thumbnail" do
+      expect(solr_doc["thumbnail_path_ss"]).to start_with("/assets/collection")
+    end
+  end
+end
