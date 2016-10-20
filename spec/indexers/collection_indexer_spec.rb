bb24933f431844f25d55801a034bb0b58cf35bac
Adam Wead <amsterdamos@gmail.com>
2016-10-20 13:23:49 -0400

    Using rails_helper in place of spec_helper

--- spec/indexers/collection_indexer_spec.rb
+++ spec/indexers/collection_indexer_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require 'spec_helper'
+require 'rails_helper'
 
 describe Sufia::CollectionIndexer do
   describe "fontawesome default icon" do
--- spec/rake/harvest_spec.rb
+++ spec/rake/harvest_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require 'spec_helper'
+require 'rails_helper'
 require 'rake'
 
 describe "scholarsphere:harvest" do
