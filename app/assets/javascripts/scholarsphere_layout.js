c5107d52abea3a5d8a5e0617249f416f96ccd23c
mtribone <mtribone@psu.edu>
2016-10-26 09:59:28 -0400

    Use Blacklight onLoad function to remove container class and add container-fluid class.

---
+++ app/assets/javascripts/scholarsphere_layout.js
@@ -0,0 +1,7 @@
+/**
+ * Add/remove a class to/from a DIV and not override a whole view
+ */
+
+Blacklight.onLoad(function() {
+    $( "#content-wrapper" ).removeClass( "container" ).addClass( "container-fluid" );
+});
