f54f3fdd07412da9a5e6f2bc6d62c036c39ac75a
Adam Wead <amsterdamos@gmail.com>
2017-01-05 12:03:15 -0500

    Upload local files with spaces remotely, ref #447

---
+++ app/actors/sufia/create_with_remote_files_actor.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+# Overrides Sufia. Remove this file when we've updated to Sufia 7.3
+module Sufia
+  # Attaches remote files to the work
+  class CreateWithRemoteFilesActor < CurationConcerns::Actors::AbstractActor
+    def create(attributes)
+      remote_files = attributes.delete(:remote_files)
+      next_actor.create(attributes) && attach_files(remote_files)
+    end
+
+    def update(attributes)
+      remote_files = attributes.delete(:remote_files)
+      next_actor.update(attributes) && attach_files(remote_files)
+    end
+
+    protected
+
+      # @param [HashWithIndifferentAccess]
+      # @return [TrueClass]
+      def attach_files(remote_files)
+        return true unless remote_files
+        remote_files.each do |file_info|
+          next if file_info.blank? || file_info[:url].blank?
+          create_file_from_url(file_info[:url], file_info[:file_name])
+        end
+        true
+      end
+
+      # Generic utility for creating FileSet from a URL
+      # Used in to import files using URLs from a file picker like browse_everything
+      def create_file_from_url(url, file_name)
+        ::FileSet.new(import_url: url, label: file_name) do |fs|
+          actor = CurationConcerns::Actors::FileSetActor.new(fs, user)
+          actor.create_metadata(curation_concern, visibility: curation_concern.visibility)
+          fs.save!
+          uri = URI.parse(URI.encode(url))
+          if uri.scheme == 'file'
+            IngestLocalFileJob.perform_later(fs, URI.decode(uri.path), user)
+          else
+            ImportUrlJob.perform_later(fs, log(actor.user))
+          end
+        end
+      end
+
+      def log(user)
+        CurationConcerns::Operation.create!(user: user,
+                                            operation_type: "Attach Remote File")
+      end
+  end
+end
---
+++ spec/actors/sufia/create_with_remote_files_actor_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe Sufia::CreateWithRemoteFilesActor do
+  let(:user) { create(:user) }
+  let(:work) { create(:work, user: user) }
+
+  let(:create_actor) do
+    double('create actor', create: true,
+                           curation_concern: work,
+                           user: user)
+  end
+  let(:actor) do
+    CurationConcerns::Actors::ActorStack.new(work, user, [described_class])
+  end
+
+  let(:remote_files) do
+    [{ url: "file:///local/file/ pigs .txt",
+       expires: "2014-03-31T20:37:36.214Z",
+       file_name: "here.txt" }]
+  end
+
+  let(:attributes) { { remote_files: remote_files } }
+
+  before do
+    allow(CurationConcerns::Actors::RootActor).to receive(:new).and_return(create_actor)
+    allow(create_actor).to receive(:create).and_return(true)
+  end
+
+  context "with local files" do
+    it "attaches files with spaces" do
+      expect(IngestLocalFileJob).to receive(:perform_later).with(FileSet, "/local/file/ pigs .txt", user)
+      expect(actor.create(attributes)).to be true
+    end
+  end
+end
