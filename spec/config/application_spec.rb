39b1acdea4133b3eb27345ca44231d0a98fb1835
Adam Wead <amsterdamos@gmail.com>
2017-02-10 14:32:41 -0500

    Move scholarsphere.yml to application.yml, ref #554

--- spec/config/application_spec.rb
+++ spec/config/application_spec.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-describe "Environment variable configuration" do
-  it "uses application.yml to add variables and values" do
-    expect(ENV["useless-variable"]).to eq("foo")
+describe "Application configuration" do
+  let(:config) { Rails.application.config }
+
+  describe "ffmpeg_path" do
+    subject { config.ffmpeg_path }
+    it { is_expected.to eq("ffmpeg-test") }
+  end
+
+  describe "service_instance" do
+    subject { config.service_instance }
+    it { is_expected.to eq("example-test") }
+  end
+
+  describe "virtual_host" do
+    subject { config.virtual_host }
+    it { is_expected.to eq("http://test.com/") }
+  end
+
+  describe "stats_email" do
+    subject { config.stats_email }
+    it { is_expected.to eq("Test email") }
+  end
+
+  describe "derivatives path" do
+    subject { config.derivatives_path }
+    it { is_expected.to end_with("tmp/derivatives") }
+  end
+
+  describe "Google analytics ID" do
+    subject { config.google_analytics_id }
+    it { is_expected.to eq("test-id") }
   end
 end
