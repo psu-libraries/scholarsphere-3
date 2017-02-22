2990be5bcbd99056a1b1cbad7a6e853297d3fb5b
Adam Wead <amsterdamos@gmail.com>
2017-02-22 14:59:05 -0500

    Remove extraneous spaces from attributes, ref #666

---
+++ app/forms/concerns/with_cleaner_attributes.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+module WithCleanerAttributes
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def model_attributes(form_params)
+      cleaner_params = super
+      terms.each do |key|
+        if cleaner_params[key].is_a?(Array)
+          cleaner_params[key].map!(&:squish)
+        elsif cleaner_params[key].is_a?(String)
+          cleaner_params[key] = cleaner_params[key].squish
+        end
+      end
+      cleaner_params
+    end
+  end
+end
