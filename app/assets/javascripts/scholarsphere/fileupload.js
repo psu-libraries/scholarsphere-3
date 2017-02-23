d93555e6e1c714c68801a12adaa4a5c5f6fd5735
Adam Wead <amsterdamos@gmail.com>
2017-02-23 13:55:21 -0500

    Display message after clicking Save, ref #643

--- app/assets/javascripts/scholarsphere/fileupload.js
+++ app/assets/javascripts/scholarsphere/fileupload.js
@@ -5,4 +5,8 @@ Blacklight.onLoad(function() {
   $(":input").focusout(function () {
     $(this).removeClass("has-focus");
   });
+
+  $("#new_generic_work, #new_batch_upload_item, .edit_generic_work").on('submit', function(event) {
+    $(".panel-footer").toggleClass("hidden");
+  });
 });
