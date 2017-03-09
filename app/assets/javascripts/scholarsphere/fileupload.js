316b2b0e7ebaa9c6c7bde527daed2b159f8c658c
Adam Wead <amsterdamos@gmail.com>
2017-03-09 10:01:57 -0500

    Enable changing text for requirements, ref #640
    Changes the text of the requirements links so that at screen reader informs the user as to whether the requirements are met.
    
    Additionally, the links themselves need some Javascript reworking so they would active the tabs when clicked.

--- app/assets/javascripts/scholarsphere/fileupload.js
+++ app/assets/javascripts/scholarsphere/fileupload.js
@@ -9,4 +9,24 @@ Blacklight.onLoad(function() {
   $("#new_generic_work, #new_batch_upload_item, .edit_generic_work").on('submit', function(event) {
     $(".panel-footer").toggleClass("hidden");
   });
+
+  // Using Bootstrap's tab links won't work if they're outside of the tabs they're controlling.
+  // Here we do it "manually"
+  $('.tabfaker').on('click', function(event) {
+    var metadata_tab = $("ul.nav-tabs li")[0];
+    var files_tab = $("ul.nav-tabs li")[1];
+
+    $("ul.nav-tabs li").removeClass("active");
+    $("div.tab-content div").removeClass("active");
+
+    if ($(this).attr("href") == "#metadata") {
+      $(metadata_tab).addClass("active");
+      $("div#metadata").addClass("active");
+    }
+
+    if ($(this).attr("href") == "#files"){
+      $(files_tab).addClass("active");
+      $("div#files").addClass("active");
+    }
+  });
 });
