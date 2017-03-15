523f61bb845f72268f5841f9a2059e6b2d79f73d
Justin Patterson <jrp22@psu.edu>
2017-03-15 11:41:01 -0400

    Rubocop syntax fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 #
 # Cookbook:: scholarsphere_web
 # Recipe:: deploy
@@ -10,80 +11,79 @@
 include_recipe 'common_library::deploy'
 
 %w(/opt/heracles /dlt).each do |dir_name|
-    directory dir_name do
-        owner 'root'
-        group 'root'
-        mode 0o755
-        action :create
-    end
+  directory dir_name do
+    owner 'root'
+    group 'root'
+    mode 0o755
+    action :create
+  end
 end
 
 template "#{node[:deploy][:home]}/.bashrc" do
-    source 'bashrc.erb'
-    owner 'deploy'
-    group 'deploy'
-    mode '0644'
+  source 'bashrc.erb'
+  owner 'deploy'
+  group 'deploy'
+  mode '0644'
 end
 
 template "#{node[:deploy][:home]}/.bash_profile" do
-    source 'bash_profile.erb'
-    owner 'deploy'
-    group 'deploy'
-    mode '0644'
+  source 'bash_profile.erb'
+  owner 'deploy'
+  group 'deploy'
+  mode '0644'
 end
 
 directory "#{node[:deploy][:home]}/.ssh" do
-    owner 'deploy'
-    group 'deploy'
-    mode '2755'
-    action :create
+  owner 'deploy'
+  group 'deploy'
+  mode '2755'
+  action :create
 end
 
 template "#{node[:deploy][:home]}/.ssh/config" do
-    source 'ssh_config.erb'
-    owner 'deploy'
-    group 'deploy'
-    mode '0644'
+  source 'ssh_config.erb'
+  owner 'deploy'
+  group 'deploy'
+  mode '0644'
 end
 
 template "#{node[:deploy][:home]}/.ssh/authorized_keys" do
-    source 'authorized_keys.erb'
-    owner 'deploy'
-    group 'deploy'
-    mode '0600'
+  source 'authorized_keys.erb'
+  owner 'deploy'
+  group 'deploy'
+  mode '0600'
 end
 
 template "#{node[:deploy][:home]}/.ssh/id_github_rsa.pub" do
-    source 'id_github_rsa.pub.erb'
-    owner 'deploy'
-    group 'deploy'
-    mode '0644'
+  source 'id_github_rsa.pub.erb'
+  owner 'deploy'
+  group 'deploy'
+  mode '0644'
 end
 
 #
 # application directories
 directory "#{node[:deploy][:home]}/#{node[:application]}" do
-    owner 'deploy'
-    group 'deploy'
-    mode '0775'
-    action :create
+  owner 'deploy'
+  group 'deploy'
+  mode '0775'
+  action :create
 end
 
 directory "#{node[:deploy][:home]}/#{node[:application]}/shared" do
-    owner 'deploy'
-    group 'deploy'
-    mode '0775'
-    action :create
+  owner 'deploy'
+  group 'deploy'
+  mode '0775'
+  action :create
 end
 
 directory "#{node[:deploy][:home]}/#{node[:application]}/shared/log" do
-    owner 'deploy'
-    group 'deploy'
-    mode '0775'
-    action :create
+  owner 'deploy'
+  group 'deploy'
+  mode '0775'
+  action :create
 end
 
 link "#{node[:deploy][:home]}/#{node[:application]}/shared/config" do
-    to "/#{node[:application]}/config_#{node[:environment]}/#{node[:application]}"
+  to "/#{node[:application]}/config_#{node[:environment]}/#{node[:application]}"
 end
-
