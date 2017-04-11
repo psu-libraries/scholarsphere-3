psu-libraries/scholarsphere-3

commit 8094bfb9d5fe4d1daf18f212d4778923786d72ba

Download remote files using their original name, ref #532

app/jobs/import_url_job.rb
@@ -1,0 +1,49 @@
+# frozen_string_literal: true
+require 'uri'
+require 'tempfile'
+require 'browse_everything/retriever'
+
+class ImportUrlJob < ActiveJob::Base
+  queue_as CurationConcerns.config.ingest_queue_name
+
+  before_enqueue do |job|
+    log = job.arguments.last
+    log.pending_job(job)
+  end
+
+  def perform(file_set, file_name, log)
+    log.performing!
+    user = User.find_by_user_key(file_set.depositor)
+
+    File.open(File.join(Dir.tmpdir, file_name), "w+") do |f|
+      copy_remote_file(file_set, f)
+
+      # reload the FileSet once the data is copied since this is a long running task
+      file_set.reload
+
+      # attach downloaded file to FileSet stubbed out
+      if CurationConcerns::Actors::FileSetActor.new(file_set, user).create_content(f)
+        # send message to user on download success
+        CurationConcerns.config.callback.run(:after_import_url_success, file_set, user)
+        log.success!
+      else
+        CurationConcerns.config.callback.run(:after_import_url_failure, file_set, user)
+        log.fail!(file_set.errors.full_messages.join(' '))
+      end
+    end
+  end
+
+  protected
+
+    def copy_remote_file(file_set, f)
+      f.binmode
+      # download file from url
+      uri = URI(file_set.import_url)
+      spec = { 'url' => uri }
+      retriever = BrowseEverything::Retriever.new
+      retriever.retrieve(spec) do |chunk|
+        f.write(chunk)
+      end
+      f.rewind
+    end
+end

spec/jobs/import_url_job_spec.rb
@@ -1,0 +1,22 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe ImportUrlJob do
+  let(:user)           { create(:user) }
+  let(:file_set)       { create(:file_set, user: user, import_url: "import_url") }
+  let(:log)            { double }
+  let(:mock_retriever) { double }
+
+  before do
+    allow(log).to receive(:performing!)
+    allow(BrowseEverything::Retriever).to receive(:new).and_return(mock_retriever)
+    allow(mock_retriever).to receive(:retrieve)
+    allow(Hydra::Works::VirusCheckerService).to receive(:file_has_virus?).and_return(false)
+  end
+
+  it "uses the file's original filename" do
+    expect(CurationConcerns.config.callback).to receive(:run).with(:after_import_url_success, file_set, user)
+    expect(log).to receive(:success!)
+    described_class.perform_now(file_set, "file_name", log)
+  end
+end

app/actors/sufia/create_with_remote_files_actor.rb
@@ -37,7 +37,7 @@ module Sufia
           if uri.scheme == 'file'
             IngestLocalFileJob.perform_later(fs, URI.decode(uri.path), user)
           else
-            ImportUrlJob.perform_later(fs, log(actor.user))
+            ImportUrlJob.perform_later(fs, file_name, log(actor.user))
           end
         end
       end

commit dc7a84e05bd5e8fb7b9276ac19f291734918d294

Sanitize remote file names, ref #727
This problem was introduced in 8094bfb9d5fe4d1daf18f212d4778923786d72ba

Remotely-uploaded files from BrowseEverything were failing to process because of non-alphanumeric characters in their filenames, such as
spaces and parentheses.

CarrierWave, which is responsible for uploading local files for ingest, already has a process for sanitizing filenames by removing these
unsupported characters. We can fix the remote file problem by using CarrierWave::SanitizedFile to clean remote filenames the same way
we are cleaning the local ones.

This increases our dependency on CarrierWave, but we're already using it, so this bring both file upload processes closer inline with
one another.

app/jobs/import_url_job.rb
@@ -11,11 +11,19 @@ class ImportUrlJob < ActiveJob::Base
     log.pending_job(job)
   end
 
+  # @param [FileSet] file_set
+  # @param [String] file_name
+  # @param [CurationConcerns::Operation] log to send messages
+  # Overrides the CurationConcerns job to accept the remote file's original name. The file
+  # is downloaded using its original name and extension, but sanitized with CarrierWave to
+  # remove any non-alphanumeric characters. This is the same process that occurs with locally
+  # uploaded files (via CarrierWave) and avoids problems later when interacting with filenames
+  # that have unsupported characters.
   def perform(file_set, file_name, log)
     log.performing!
     user = User.find_by_user_key(file_set.depositor)
 
-    File.open(File.join(Dir.tmpdir, file_name), "w+") do |f|
+    File.open(File.join(Dir.tmpdir, CarrierWave::SanitizedFile.new(file_name).filename), "w+") do |f|
       copy_remote_file(file_set, f)
 
       # reload the FileSet once the data is copied since this is a long running task

spec/jobs/import_url_job_spec.rb
@@ -6,6 +6,7 @@ describe ImportUrlJob do
   let(:file_set)       { create(:file_set, user: user, import_url: "import_url") }
   let(:log)            { double }
   let(:mock_retriever) { double }
+  let(:file_name)      { "Development Team Projects and Milestones (not downloaded).xlsx" }
 
   before do
     allow(log).to receive(:performing!)
@@ -14,9 +15,10 @@ describe ImportUrlJob do
     allow(Hydra::Works::VirusCheckerService).to receive(:file_has_virus?).and_return(false)
   end
 
-  it "uses the file's original filename" do
+  it "sanitizes the file name" do
     expect(CurationConcerns.config.callback).to receive(:run).with(:after_import_url_success, file_set, user)
     expect(log).to receive(:success!)
-    described_class.perform_now(file_set, "file_name", log)
+    described_class.perform_now(file_set, file_name, log)
+    expect(file_set.label).to eq("Development_Team_Projects_and_Milestones__not_downloaded_.xlsx")
   end
 end