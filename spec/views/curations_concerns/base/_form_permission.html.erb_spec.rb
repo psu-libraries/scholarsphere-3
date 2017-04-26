30bc0562e8d96d6621149e777c5ea8e23bc7c49b
Adam Wead <amsterdamos@gmail.com>
2017-04-26 11:19:00 -0400

    Remove private visibility option, #779
    Holding works in a private status will no longer be an option. Therefore, the option must be removed.
    
    Existing private works will remain private and editing them will continue to allow them to be private even though the
    option is no longer explicitly present. New works can be embargoed, and collections will have no private option at all.

---
+++ spec/views/curations_concerns/base/_form_permission.html.erb_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe "curation_concerns/base/_form_permission.html.erb" do
+  let(:work) { build(:work) }
+  let(:form) { CurationConcerns::GenericWorkForm.new(work, Ability.new(nil)) }
+
+  let(:page) {
+    view.simple_form_for(form) do |f|
+      render 'curation_concerns/base/form_permission.html.erb', f: f
+    end
+    Capybara::Node::Simple.new(rendered)
+  }
+
+  it "omits the private visibility option" do
+    expect(page).to have_no_checked_field("Private")
+  end
+end
