50161dec9c678157a7849d189ff1322915fa4656
Adam Wead <amsterdamos@gmail.com>
2017-05-17 15:47:02 -0400

    Render a telephone link in user profile, ref #801
    Current links to telephone numbers were not doing anything. This allows the browser to trigger any configured
    applications to dial the number.

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -16,6 +16,8 @@ describe 'users/show.html.erb', type: :view do
     render
   end
 
+  subject { rendered }
+
   describe "when the user doesn't have a title" do
     let(:user) { build(:user, title: nil, created_at: join_date) }
 
@@ -32,5 +34,10 @@ describe 'users/show.html.erb', type: :view do
       expect(rendered).to match(/<dt>Title<\/dt>/)
       expect(rendered).to match(/<dd>Mrs<\/dd>/)
     end
+  end
+
+  describe "when user has a phone number" do
+    let(:user) { build(:user, created_at: join_date, telephone: "+1 800 867 5309") }
+    it { is_expected.to include('<a href="tel:+18008675309">+1 800 867 5309</a>') }
   end
 end
