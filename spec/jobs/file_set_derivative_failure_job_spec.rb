19ca129891f5db172fffab891b21cadd406e89ab
Adam Wead <amsterdamos@gmail.com>
2017-05-24 15:53:42 -0400

    Inform user when derivative creation fails for files, ref #706
    Not all files are supported by ImageMagick and when it is not able to create thumbnails for a file, it fails in
    the resque queue, but the user never sees anything.
    
    This reports a derivative failure to the file set's activity stream so the user knows that a failure occurred.

---
+++ spec/jobs/file_set_derivative_failure_job_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe FileSetDerivativeFailureJob do
+  let(:user)     { create(:user) }
+  let(:file_set) { work.file_sets.first }
+
+  let!(:work)    { create(:work, :with_one_file, user: user) }
+
+  it "sends an failed derivative message on a file set" do
+    expect {
+      described_class.perform_now(file_set, user)
+    }.to change { file_set.events.length }.by(1)
+  end
+end
