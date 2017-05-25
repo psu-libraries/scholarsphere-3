714118b77379b0dd2c50cbdc7ef44db49f1c4cac
Adam Wead <amsterdamos@gmail.com>
2017-05-25 13:33:36 -0400

    Override LocalFileJob to set file set's title to filename, ref #876
    When uploading new versions of files, the file set's title retained the name of the first version of the file. If another version
    of the file was uploaded using a different name, the file set's title wasn't updated.
    
    Overriding CurationConcerns LocalFileJob to update the file set title with the file name fixes this issue. Additionally,
    the :after_revert_content callback is used to ensure the title and label are changed if the file set is reverted
    to a previous version.

---
+++ spec/jobs/ingest_file_job_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe IngestFileJob do
+  let(:user)     { create(:user) }
+  let(:file_set) { create(:file_set, :with_png, depositor: user.login) }
+  let(:filepath) { File.join(fixture_path, "world.png") }
+
+  context "when adding a new version" do
+    before { allow(CharacterizeJob).to receive(:perform_later) }
+
+    it "updates the file set's title with the file name" do
+      expect(file_set.title).to contain_exactly("Sample PNG")
+      described_class.perform_now(file_set, filepath, user)
+      expect(file_set.title).to contain_exactly("world.png")
+    end
+  end
+end
