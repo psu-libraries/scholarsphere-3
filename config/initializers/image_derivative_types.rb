5eca5cec676fb1ec56fd5ff62e92015a4649df9e
Carolyn Cole <cam156@psu.edu>
2017-05-26 12:58:09 -0400

    overriding the image derivative types to not include tiff

---
+++ config/initializers/image_derivative_types.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# Override image mime types so we do not process tiff files since these are causing issues
+#   currently the tiff files in the system are geo tiffs which just hang up the derivative generation
+module Hydra::Works::MimeTypes::ClassMethods
+  def image_mime_types
+    ['image/png', 'image/jpeg', 'image/jpg', 'image/jp2', 'image/bmp', 'image/gif']
+  end
+end
