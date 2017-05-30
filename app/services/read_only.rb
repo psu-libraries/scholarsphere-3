2b46901fd044d175b807b71521a3076f23e3f8df
Carolyn Cole <cam156@psu.edu>
2017-05-30 12:56:43 -0400

    Handle the fact that figaro converts everything to a string so we may get a boolean or string value

--- app/services/read_only.rb
+++ app/services/read_only.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+
 class ReadOnly
   class << self
     def read_only?
-      ScholarSphere::Application.config.respond_to?(:read_only) && ScholarSphere::Application.config.read_only
+      ScholarSphere::Application.config.respond_to?(:read_only) && config_boolean
     end
 
     def announcement_text
@@ -19,6 +20,13 @@ class ReadOnly
 
       def default_read_only_announcement
         'The system is currently in read only mode for maintenance. Please try again later to upload or modify your ScholarSphere content.'
+      end
+
+      def config_boolean
+        config_value = ScholarSphere::Application.config.read_only
+        return config_value if config_value.class == TrueClass || @config_value == FalseClass
+
+        config_value.to_s.casecmp("true").zero?
       end
   end
 end
--- spec/services/read_only_spec.rb
+++ spec/services/read_only_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require 'spec_helper'
+
+require 'rails_helper'
 
 describe ReadOnly do
   describe "#read_only?" do
@@ -21,6 +22,20 @@ describe ReadOnly do
     context "read only flag true" do
       before do
         allow(ScholarSphere::Application.config).to receive(:read_only).and_return(true)
+      end
+      it { is_expected.to be_truthy }
+    end
+
+    context "read only flag 'false'" do
+      before do
+        allow(ScholarSphere::Application.config).to receive(:read_only).and_return('false')
+      end
+      it { is_expected.to be_falsey }
+    end
+
+    context "read only flag 'true'" do
+      before do
+        allow(ScholarSphere::Application.config).to receive(:read_only).and_return('true')
       end
       it { is_expected.to be_truthy }
     end
