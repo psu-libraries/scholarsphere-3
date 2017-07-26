7b4802e78956e503b1ddbf685aa453fd64c9f453
Adam Wead <amsterdamos@gmail.com>
2017-07-26 16:25:50 -0400

    Rename file sets during upload errors, ref #974 & #969
    ImportUrlJob will fail if the file's url has expired, or if there is
    some other kind of error during its retrieval, such as a timeout or
    other disruption.
    
    Instead of letting the job fail and having Resque capture the error, we
    can have the job capture the error and rename the file set to the kind
    of error.
    
    The benefit is that it surfaces the error to the user so that they can
    take action: delete the file set and try again. The drawback is that
    it's still creating the resource in Fedora. Ideally, perhaps, we ought
    to capture these errors even before the file set is created and attached
    to the work. However, that would require deeper changes to the stack.

--- spec/jobs/import_url_job_spec.rb
+++ spec/jobs/import_url_job_spec.rb
@@ -3,13 +3,14 @@
 require 'rails_helper'
 
 describe ImportUrlJob do
-  let(:user)           { create(:user) }
-  let(:file_set)       { create(:file_set, user: user, import_url: 'import_url') }
-  let(:log)            { double }
-  let(:mock_retriever) { double }
-  let(:http_status) { true }
+  let(:user)             { create(:user) }
+  let(:file_set)         { create(:file_set, user: user, import_url: 'import_url') }
+  let(:log)              { double }
+  let(:mock_retriever)   { double }
+  let(:http_status)      { true }
   let(:mock_http_result) { instance_double('HTTParty::Response', success?: http_status) }
-  let(:file_name) { 'Development Team Projects and Milestones (not downloaded).xlsx' }
+  let(:file_name)        { 'Development Team Projects and Milestones (not downloaded).xlsx' }
+  let(:inbox)            { user.mailbox.inbox }
 
   before do
     allow(log).to receive(:performing!)
@@ -19,25 +20,42 @@ describe ImportUrlJob do
     allow(HTTParty).to receive(:head).and_return(mock_http_result)
   end
 
-  it 'sanitizes the file name' do
-    expect(CurationConcerns.config.callback).to receive(:run).with(:after_import_url_success, file_set, user)
-    expect(log).to receive(:success!)
-    described_class.perform_now(file_set, file_name, log)
-    expect(file_set.label).to eq('Development_Team_Projects_and_Milestones__not_downloaded_.xlsx')
+  context 'with a successful import' do
+    it 'attaches a sanitized the file name to the file set' do
+      expect(CurationConcerns.config.callback).to receive(:run).with(:after_import_url_success, file_set, user)
+      expect(log).to receive(:success!)
+      described_class.perform_now(file_set, file_name, log)
+      expect(file_set.label).to eq('Development_Team_Projects_and_Milestones__not_downloaded_.xlsx')
+    end
   end
 
-  context 'http head fails' do
+  context 'when the remote file is unavailable' do
     let(:http_status) { false }
-    let(:inbox) { user.mailbox.inbox }
 
-    it 'fails to add the content' do
+    it 'logs the error and renames the file set' do
       expect(log).to receive(:fail!)
       expect(file_set.original_file).to be_nil
       described_class.perform_now(file_set, file_name, log)
+      expect(file_set.title).to eq(['Upload failed for Development Team Projects and Milestones (not downloaded).xlsx. Please delete and upload again'])
       expect(inbox.count).to eq(1)
       last_message = inbox[0].last_message
       expect(last_message.subject).to eq('File Import Error')
-      expect(last_message.body).to eq("Error Downloading Content for <a href=\"/concern/file_sets/#{file_set.id}\">Development Team Projects and Milestones (not downloaded).xlsx</a>")
+      expect(last_message.body).to eq("Error: Could not retrieve file for <a href=\"/concern/file_sets/#{file_set.id}\">Development Team Projects and Milestones (not downloaded).xlsx</a>. Reason: Expired URL")
+    end
+  end
+
+  context 'when retrieval fails' do
+    before { allow(mock_retriever).to receive(:retrieve).and_raise(StandardError, 'Timeout') }
+
+    it 'logs the error and renames the file set' do
+      expect(log).to receive(:fail!)
+      expect(file_set.original_file).to be_nil
+      described_class.perform_now(file_set, file_name, log)
+      expect(file_set.title).to eq(['Upload failed for Development Team Projects and Milestones (not downloaded).xlsx. Please delete and upload again'])
+      expect(inbox.count).to eq(1)
+      last_message = inbox[0].last_message
+      expect(last_message.subject).to eq('File Import Error')
+      expect(last_message.body).to eq("Error: Could not retrieve file for <a href=\"/concern/file_sets/#{file_set.id}\">Development Team Projects and Milestones (not downloaded).xlsx</a>. Reason: Timeout")
     end
   end
 end
