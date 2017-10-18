9ba7d6d297e7ee45b9737e8b40da05c6384442fc
Carolyn Cole <cam156@psu.edu>
2017-10-18 14:36:53 -0400

    Changing resolrize to traverse the top level for ids and then index the permissions separately

--- app/jobs/resolrize_job.rb
+++ app/jobs/resolrize_job.rb
@@ -9,19 +9,24 @@ class ResolrizeJob < ActiveJob::Base
     # but this causes more requests to Fedora.
     return [] unless resource.head.rdf_source?
 
-    descendant_uris = ActiveFedora::Base.descendant_uris(ActiveFedora.fedora.base_uri)
+    descendant_uris = resource.graph.query(predicate: ::RDF::Vocab::LDP.contains).map { |descendant| descendant.object.to_s }
 
     # map uri to id to make filtering types easier
     descendant_ids = descendant_uris.map { |uri| ActiveFedora::Base.uri_to_id(uri) }.compact
 
     # models have the 10 digit ids fron AF Noid so remove the longer ones
     model_ids = descendant_ids.reject { |id| id.length > model_id_length }
+    model_ids.sort { |a, b| [b.size, b] <=> [a.size, a] }
 
     # permissions have the longer ids
     permission_ids = descendant_ids.reject { |id| id.length <= model_id_length }
 
     # run the permission ids first so that solr has the permissions before the index is updated for the model
     update_group(permission_ids)
+    puts permission_ids
+
+    # run update index on the permissions of the models
+    update_permissions(model_ids)
 
     # run the models
     update_group(model_ids)
@@ -41,8 +46,22 @@ class ResolrizeJob < ActiveJob::Base
         logger.debug "Re-index everything ... #{id}"
         begin
           ActiveFedora::Base.find(id).update_index
-        rescue
-          logger.error "error processing #{id}"
+        rescue StandardError => error
+          logger.error "error processing #{id}: #{error}"
+        end
+      end
+    end
+
+    def update_permissions(ids)
+      ids.each do |id|
+        logger.debug "Re-index everything permissions ... #{id}"
+        begin
+          ActiveFedora::Base.find(id).permissions.each do |perm|
+            puts perm.id
+            ActiveFedora::Base.find(perm.id).update_index
+          end
+        rescue StandardError => error
+          logger.error "error processing #{id}: #{error}"
         end
       end
     end
