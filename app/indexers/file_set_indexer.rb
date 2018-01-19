109010a459b993aa5dcb6d6b3a73facb0b8e5d6f
Carolyn Cole <cam156@psu.edu>
2018-01-19 09:26:22 -0500

    forcing UTF8 on all text Solr has been returning an illegal UTF to Ascii error, which we have been missing becuase it happens in the background.  This will eliminate the error

--- app/indexers/file_set_indexer.rb
+++ app/indexers/file_set_indexer.rb
@@ -6,6 +6,13 @@ class FileSetIndexer < CurationConcerns::FileSetIndexer
     super.tap do |solr_doc|
       solr_doc.delete(Solrizer.solr_name(:file_size, CurationConcerns::FileSetIndexer::STORED_INTEGER))
       solr_doc[Solrizer.solr_name(:file_size, CurationConcerns::CollectionIndexer::STORED_LONG)] = object.file_size[0]
+      if solr_doc['all_text_timv'].present?
+        begin
+          solr_doc['all_text_timv'].force_encoding('UTF-8')
+        rescue StandardError => e
+          logger.warn "could not convert File Set content to UTF8 #{object.id} #{e}"
+        end
+      end
     end
   end
 end
