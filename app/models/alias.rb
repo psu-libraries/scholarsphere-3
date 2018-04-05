e2e3b157512e0f080b96634152cf84a534a01675
Carolyn Cole <cam156@psu.edu>
2018-04-05 10:26:19 -0400

    Adding indexing of other aliases to current alias refs #992

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -7,7 +7,21 @@ class Alias < ActiveFedora::Base
 
   belongs_to :agent, class_name: 'Agent', predicate: ::RDF::Vocab::FOAF.name
 
+  after_save :update_alias_indexes
+
   property :display_name, predicate: ::RDF::Vocab::FOAF.nick, multiple: false do |index|
     index.as :stored_searchable, :symbol
+  end
+
+  def update_alias_indexes
+    others_aliases = other_aliases_for_my_agent
+    return if others_aliases.blank? || previous_changes.exclude?('display_name')
+
+    others_aliases.each(&:update_index)
+  end
+
+  def other_aliases_for_my_agent
+    return if agent.blank?
+    agent.reload.aliases.reject { |current_alias| current_alias.id == id }
   end
 end
