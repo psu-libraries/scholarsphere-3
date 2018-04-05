c10fc908242fed21417b042f5846683f834ea564
Carolyn Cole <cam156@psu.edu>
2018-04-05 10:26:19 -0400

    Adding indexing of other aliases to current alias refs #992

--- spec/indexers/alias_indexer_spec.rb
+++ spec/indexers/alias_indexer_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 describe AliasIndexer do
-  let(:agent) { build :agent, given_name: 'Sue', sur_name: 'Doe' }
-  let(:alias_item) { build :alias, id: '123abc', agent: agent }
+  let(:agent) { create :agent, given_name: 'Sue', sur_name: 'Doe' }
+  let(:alias_item) { create :alias, agent: agent }
   let(:indexer) { described_class.new(alias_item) }
 
   describe '#generate_solr_document' do
@@ -13,5 +13,15 @@ describe AliasIndexer do
     let(:solr_doc) { indexer.generate_solr_document }
 
     it { is_expected.to include('agent_name_tesim' => 'Sue Doe') }
+
+    context 'additional aliases' do
+      let(:alias2) { create :alias, agent: agent, display_name: 'mickey mouse' }
+
+      before do
+        alias_item
+        alias2
+      end
+      it { is_expected.to include('agent_name_tesim' => 'Sue Doe, mickey mouse') }
+    end
   end
 end
