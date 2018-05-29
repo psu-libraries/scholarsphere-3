14b2a79231d7fa09c3be3d950367a2babd4c4f5e
Carolyn Cole <cam156@psu.edu>
2018-05-29 13:28:42 -0400

    Adding specialized facet cleaner for creator This gets the facet value for the record not just the text Adds tests for keyword and publisher facet cleaning refs #1223

--- app/indexers/indexes_creator.rb
+++ app/indexers/indexes_creator.rb
@@ -15,11 +15,12 @@ module IndexesCreator
       creator_names = object.creators.map(&:display_name)
       facet_names = object.creators.map { |c| build_facet(c) }
       solr_doc[Solrizer.solr_name('creator_name', :facetable)] = facet_names
+      solr_doc[Solrizer.solr_name('creator_facet_name', :stored_searchable)] = facet_names
       solr_doc[Solrizer.solr_name('creator_name', :stored_searchable)] = creator_names
     end
 
     def build_facet(creator)
       return creator.display_name if creator.agent.nil?
-      "#{creator.agent.given_name} #{creator.agent.sur_name}".strip
+      "#{creator.agent.given_name} #{creator.agent.sur_name}".strip.titleize.gsub(/[\.\,]/, '')
     end
 end
