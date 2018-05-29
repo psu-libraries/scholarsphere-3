4558be6d2f7b33a690e876fec3f9e4a382f2c69e
Adam Wead <amsterdamos@gmail.com>
2018-05-29 16:37:27 -0400

    Index the README file in Solr

--- app/indexers/work_indexer.rb
+++ app/indexers/work_indexer.rb
@@ -9,6 +9,7 @@ class WorkIndexer < Sufia::WorkIndexer
       solr_doc[Solrizer.solr_name('resource_type', :facetable)] = object.resource_type
       solr_doc[Solrizer.solr_name('file_format', :stored_searchable)] = representative.file_format
       solr_doc[Solrizer.solr_name('file_format', :facetable)] = representative.file_format
+      solr_doc['readme_file_ss'] = readme_content
       solr_doc[Solrizer.solr_name(:bytes, CurationConcerns::CollectionIndexer::STORED_LONG)] = object.bytes
       SolrDocumentGroomer.call(solr_doc)
     end
@@ -20,10 +21,37 @@ class WorkIndexer < Sufia::WorkIndexer
       object.representative || NullRepresentative.new
     end
 
+    def readme_file
+      @readme_file ||= ReadmeFile.new(object.readme_file)
+    end
+
+    def readme_content
+      return unless readme_file.content?
+      readme_file.content.encode('utf-8', invalid: :replace, undef: :replace)
+    rescue StandardError => e
+      "#{I18n.t('scholarsphere.generic_work.readme_error')}: #{e}"
+    end
+
     # Use the naught gem if this gets bigger
     class NullRepresentative
       def file_format
         nil
+      end
+    end
+
+    class ReadmeFile
+      attr_reader :file
+
+      def initialize(file)
+        @file = file
+      end
+
+      def content?
+        !file.nil? && file.original_file.respond_to?(:content)
+      end
+
+      def content
+        file.original_file.content
       end
     end
 end
