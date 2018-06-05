e4fa9f4f4951df79a65c1e8716ab508c24fc1baf
mtribone <mtribone@psu.edu>
2018-06-05 14:43:38 -0400

    Reorders the metadata fields to match the order of the attribute rows from the work show partial.

--- app/presenters/collection_presenter.rb
+++ app/presenters/collection_presenter.rb
@@ -4,9 +4,8 @@ class CollectionPresenter < Sufia::CollectionPresenter
   delegate :subtitle, to: :solr_document
 
   def self.terms
-    [:creator, :keyword, :size, :total_items, :resource_type, :contributor,
-     :rights, :publisher, :date_created, :subject, :language, :identifier,
-     :based_near, :related_url, :date_modified, :date_uploaded]
+    [:creator, :keyword, :rights, :resource_type, :contributor, :publisher, :date_created, :date_uploaded,
+     :date_modified, :subject, :language, :identifier, :based_near, :related_url, :size, :total_items]
   end
 
   def permission_badge_class
