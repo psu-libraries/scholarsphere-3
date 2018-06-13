229f6da3cb6700d41f38aeb6de06f0e4c36b47e8
Carolyn Cole <cam156@psu.edu>
2018-06-13 09:41:05 -0400

    Putting unicorn output to a log file instead of the screen

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 worker_processes 3
+
+# By default, the Unicorn logger will write to stderr.
+# Additionally, ome applications/frameworks log to stderr or stdout,
+# so prevent them from going to /dev/null when daemonized here:
+stderr_path 'log/unicorn.stderr.log'
+stdout_path 'log/unicorn.stdout.log'
