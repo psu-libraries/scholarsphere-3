391068d6d040d81c97ae7ad808800adac3168a5b
Carolyn Cole <cam156@psu.edu>
2018-08-27 13:05:01 -0400

    Adding index of work for readme Updated tests to include a file with the readme to make them more realistic Updated the parent_needs_reindex? method to include reindex for readme addition removed additional updates to the index that were no longer needed in the test

--- spec/features/generic_work/work_with_readme_spec.rb
+++ spec/features/generic_work/work_with_readme_spec.rb
@@ -3,12 +3,10 @@
 require 'feature_spec_helper'
 
 describe GenericWork do
-  let!(:current_user) { create(:user) }
-  let!(:work) { create(:public_work_with_readme, title: ['Work with README'], depositor: current_user.login) }
+  let(:current_user) { create(:user) }
+  let(:work) { create(:public_work_with_readme, title: ['Work with README'], depositor: current_user.login) }
 
   before do
-    described_class.all.map(&:update_index)
-    FileSet.all.map(&:update_index)
     sign_in(current_user)
   end
 
