0632509f0dac5d120b3c91e49432aa732dda41b2
Jim Campbell <jkc103@psu.edu>
2018-10-19 18:23:16 +0000

    NRDM - added New Relic Deploy Markers

--- config/deploy/prod_new.rb
+++ config/deploy/prod_new.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+ENV['NEW_RELIC_APP_NAME'] = 'ss-prod-ruby'
+
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary server in each group
