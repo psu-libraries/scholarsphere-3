238704f28874844279098ca79b349d9f88832a33
Carolyn Cole <cam156@psu.edu>
2018-11-14 13:53:14 -0500

    Combine most of the different collection tests into a singular file.
    Added edit and delete to the end of the create empty collection test
    Added search to the end of the test created with existing works
    
    Co-authored-by: Adam Wead <amsterdamos@gmail.com>
    Co-authored-by: Dan Coughlin <dan.coughlin@gmail.com>

--- spec/features/collection/create_edit_delete_spec.rb
+++ spec/features/collection/create_edit_delete_spec.rb
@@ -2,12 +2,16 @@
 
 require 'feature_spec_helper'
 
-include Selectors::Dashboard
-
 describe Collection, type: :feature, js: true do
+  include Selectors::Dashboard
+
   let(:current_user) { create(:user, display_name: 'Jill User') }
   let(:title)        { 'Test Collection Title' }
   let(:subtitle)     { 'Machu Picchu' }
+  let(:description)  { 'Stuff that I am describing' }
+  let(:updated_title)        { 'Updated Title' }
+  let(:updated_subtitle)     { 'Updated Vimana2' }
+  let(:updated_description)  { 'Updated description text.' }
 
   before { login_as(current_user) }
 
@@ -38,7 +42,7 @@ describe Collection, type: :feature, js: true do
 
       fill_in 'Title', with: title
       fill_in 'Subtitle', with: subtitle
-      fill_in 'Description', with: 'description'
+      fill_in 'Description', with: description
       fill_in 'Keyword', with: 'keyword'
       within('div#share') do
         expect(page).to have_content('Add Group')
@@ -58,12 +62,61 @@ describe Collection, type: :feature, js: true do
           expect(page).to have_content(subtitle)
         end
       end
-      expect(page).to have_content('https://doi.org')
+      expect(page).to have_selector("a[href='https://doi.org/abc123']")
 
       # The link to the creator search should look like this
       # with the correct solr key 'creator_name_sim':
       # catalog?f[creator_name_sim][]=Jill+User
       expect(find_link('Jill User')[:href]).to match /catalog\?f%5Bcreator_name_sim%5D%5B%5D=Jill\+User/
+
+      # Test Edit
+      click_link 'Edit'
+      expect(page).to have_field 'collection_title', with: title
+      expect(page).to have_field 'collection_subtitle', with: subtitle
+      expect(page).to have_field 'collection_description', with: description
+
+      within('div#share') do
+        select 'umg/up.dlt.scholarsphere-users', from: 'new_group_name_skel'
+        select 'Edit', from: 'new_group_permission_skel'
+        page.find('#add_new_group_skel').click
+        expect(page).to have_selector("input[value='umg/up.dlt.scholarsphere-users']", visible: false)
+      end
+
+      click_link 'Additional Fields'
+      within('div.collection_date_created') do
+        expect(page).to have_content('Published Date')
+        fill_in 'Published Date', with: 'Date Published'
+      end
+      expect(page).to have_checked_field('Public')
+      expect(page).to have_no_checked_field('Private')
+      fill_in 'Title', with: updated_title
+      fill_in 'Subtitle', with: updated_subtitle
+      fill_in 'Description', with: updated_description
+      expect(find('.creator-first-name')['readonly']).to eq('true')
+      expect(find('.creator-last-name')['readonly']).to eq('true')
+      fill_in 'collection[creators][0][display_name]', with: 'Mdme. Dorje Trollo'
+      click_button 'Update Collection'
+      expect(page).not_to have_content title
+      expect(page).not_to have_content description
+      expect(page).to have_content updated_title
+      expect(page).to have_content updated_subtitle
+      expect(page).to have_content updated_description
+      expect(page).to have_content 'Mdme. Dorje Trollo'
+      within('dl.metadata-collections') do
+        expect(page).to have_content('Published Date')
+      end
+
+      expect(Collection.last.edit_groups).to contain_exactly('umg/up.dlt.scholarsphere-users')
+
+      # Test Delete
+      accept_confirm { click_link 'Delete' }
+      expect(page).to have_content 'Collection was successfully deleted'
+      within('#my_nav') do
+        expect(page).to have_content('My Collections')
+      end
+      within('#documents') do
+        expect(page).not_to have_content title
+      end
     end
   end
 
@@ -75,21 +128,43 @@ describe Collection, type: :feature, js: true do
       visit(new_collection_path)
       fill_in 'Title', with: title
       fill_in 'Subtitle', with: subtitle
-      fill_in 'Description', with: 'description'
+      fill_in 'Description', with: description
       fill_in 'Keyword', with: 'keyword'
+      fill_in 'collection[creators][0][display_name]', with: 'Mdme. Dorje Trollo'
       click_button 'Create Collection and Add Existing Works'
       expect(page).to have_content('Collection was successfully created.')
       check 'check_all'
       click_button "Add to #{title}"
+
+      # Test the view
       expect(page).to have_content(title)
       within('dl.metadata-collections') do
-        expect(page).to have_content('Total Items')
-        expect(page).to have_content('2')
+        expect(page).to have_content('Total Items 2')
+        expect(page).to have_content('Size 0 Bytes')
       end
       within('table.table-striped') do
         expect(page).to have_content('First file')
         expect(page).to have_content('Second file')
       end
+      expect(page).to have_selector("a[href='/catalog?f%5Bcreator_name_sim%5D%5B%5D=Jill+User']")
+
+      within('div.actions-controls-collections') do
+        expect(page).to have_content('Download Collection as Zip')
+      end
+
+      # Test the search within the collection
+      fill_in 'collection_search', with: file1.title.first
+      click_button 'collection_submit'
+      expect(page).to have_content 'Search Results within this Collection'
+      expect(page).to have_content title
+      expect(page).to have_content description
+
+      # Should have search results / contents listing
+      expect(page).to have_content file1.title.first
+      expect(page).not_to have_content file2.title.first
+
+      # Should not have Collection Descriptive metadata table
+      expect(page).not_to have_selector 'dl.metadata-collections'
     end
   end
 
--- spec/features/collection/view_and_search_spec.rb
+++ spec/features/collection/view_and_search_spec.rb
@@ -26,45 +26,6 @@ describe Collection, type: :feature, js: true do
     create(:private_file, :with_one_file_and_size, title: ['little_file.txt'], depositor: current_user.login)
   end
 
-  context 'with a logged in user' do
-    before do
-      login_as(current_user)
-      visit("/collections/#{collection.id}")
-    end
-
-    it 'shows the collection and searches within it' do
-      expect(page).to have_content collection.title.first
-      expect(page).to have_content collection.description.first
-      expect(page).to have_content collection.creator.first.display_name
-      expect(page).to have_selector("a[href='/catalog?f%5Bcreator_name_sim%5D%5B%5D=Given+Name+Sur+Name']")
-      expect(page).to have_selector("a[href='https://doi.org/blah-blah']")
-      expect(page).to have_content file1.title.first
-      expect(page).to have_content file2.title.first
-      expect(page).to have_content 'Total Items 2'
-      expect(page).to have_content 'Size 2 Bytes'
-
-      within('div.actions-controls-collections') do
-        expect(page).to have_content('Download Collection as Zip')
-      end
-
-      within('dl.metadata-collections') do
-        expect(page).to have_content('Published Date')
-      end
-
-      fill_in 'collection_search', with: file1.title.first
-      click_button 'collection_submit'
-      expect(page).to have_content collection.title.first
-      expect(page).to have_content collection.description.first
-
-      # Should have search results / contents listing
-      expect(page).to have_content file1.title.first
-      expect(page).not_to have_content file2.title.first
-
-      # Should not have Collection Descriptive metadata table
-      expect(page).not_to have_content collection.creator.first.display_name
-    end
-  end
-
   context 'with a public user' do
     it 'displays the collection and only public files' do
       visit "/collections/#{collection.id}"
