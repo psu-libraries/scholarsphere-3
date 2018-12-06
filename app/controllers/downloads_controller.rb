786ccf30e8ca89b9a508b59b922371649d61ead6
Carolyn Cole <cam156@psu.edu>
2018-12-06 09:00:21 -0500

    Revert "Removing external download behavior as it is no longer needed (#1489)"
    This reverts commit 9a0d015c2f1d72fa9abf91223fb7299565d29fcb.

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,6 +2,9 @@
 
 class DownloadsController < ApplicationController
   include CurationConcerns::DownloadBehavior
+  if ENV['REPOSITORY_EXTERNAL_FILES'] == 'true'
+    include ExternalDownloadBehavior
+  end
 
   prepend_before_action only: [:show] do
     handle_legacy_url_prefix { |new_id| redirect_to main_app.download_path(new_id), status: :moved_permanently }
