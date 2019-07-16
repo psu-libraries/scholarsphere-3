a1c42fa20627d69561ba004bfef6d1c4c5f9155e
Dann Bohn <djb44@psu.edu>
2019-07-16 13:03:19 -0400

    unintentional added file

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$RAILS_ENV" == "production" ]; then
-    envconsul -vault-token=$(cat /etc/token/.vault-token) -secret="$VAULT_KEY_PATH" -no-prefix -once ./run.sh
-else
-    ./run.sh
-fi
